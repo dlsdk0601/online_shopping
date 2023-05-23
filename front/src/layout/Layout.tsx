d357c6021542ecf31802f5918b473d5311307394
dlsdk0601 <dlsdk0601@gmail.com>
2023-05-24 01:05:20 +0900

    refactor: 불필요한 주석 제거

--- front/src/layout/Layout.tsx
+++ front/src/layout/Layout.tsx
@@ -11,17 +11,10 @@ import { Urls } from "../url/url.g";
 export const LayoutView = (props: PropsWithChildren) => {
   return (
     <>
-      {/* <LeftSideBarView /> */}
-      {/* <div className="relative bg-blueGray-100 md:ml-64"> */}
       <HeaderView />
-      {/*  <Cards /> */}
-      {/*  <div className="-m-24 mx-auto w-full px-4 md:px-10"> */}
       {props.children}
       <BlockView />
       <FooterView />
-      {/*  </div> */}
-      {/* </div> */}
-      {/* <BlockView /> */}
     </>
   );
 };
