ec57a038ef31fb033dfb93f0458b5073607ac0d3
dlsdk0601 <dlsdk0601@gmail.com>
2023-05-23 23:41:21 +0900

    refactor: css 파일 import

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,11 +1,18 @@
+import "../styles/flex-slider.css";
+import "../styles/font-awesome.css";
+import "../styles/lightbox.css";
+import "../styles/owl-carousel.css";
+import "../styles/templatemo-hexashop.css";
+import "../styles/bootstrap.min.css";
 import "../styles/globals.css";
 import dynamic from "next/dynamic";
 import type { AppProps, AppType } from "next/app";
 import { App } from "../layout/App";
 
-const NextApp: AppType = (props: AppProps) => <App {...props} />;
+const NextApp: AppType = (props: AppProps) => {
+  return <App {...props} />;
+};
 
 // SSR 은 일단 비활성화한다. 필요에 따라서 활성화시킨다.
-const DynamicApp = dynamic(() => Promise.resolve(NextApp), { ssr: false });
 // noinspection JSUnusedGlobalSymbols
-export default DynamicApp;
+export default dynamic(() => Promise.resolve(NextApp), { ssr: false });
