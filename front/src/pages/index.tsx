53046a4deb037c8720b2b61bf3a7bfd65831a284
dlsdk0601 <dlsdk0601@gmail.com>
2023-05-28 01:08:04 +0900

    refactor: 메인 페이지 퍼블 완료

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -284,6 +284,492 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <section className="section" id="women">
+        <div className="slick-container">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="section-heading">
+                <h2>Women's Latest</h2>
+                <span>
+                  Details to details is what makes Hexashop different from the other themes.
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <Slider {...settings} className="owl-carousel owl-loaded">
+              <div className="item">
+                <div className="thumb">
+                  <div className="hover-content">
+                    <ul>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-eye"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-star"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-shopping-cart"></i>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                  <img src="/images/women-01.jpg" alt="" />
+                </div>
+                <div className="down-content">
+                  <h4>New Green Jacket</h4>
+                  <span>$75.00</span>
+                  <ul className="stars">
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="item">
+                <div className="thumb">
+                  <div className="hover-content">
+                    <ul>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-eye"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-star"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-shopping-cart"></i>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                  <img src="/images/women-02.jpg" alt="" />
+                </div>
+                <div className="down-content">
+                  <h4>Classic Dress</h4>
+                  <span>$45.00</span>
+                  <ul className="stars">
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="item">
+                <div className="thumb">
+                  <div className="hover-content">
+                    <ul>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-eye"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-star"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-shopping-cart"></i>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                  <img src="/images/women-03.jpg" alt="" />
+                </div>
+                <div className="down-content">
+                  <h4>Spring Collection</h4>
+                  <span>$130.00</span>
+                  <ul className="stars">
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="item">
+                <div className="thumb">
+                  <div className="hover-content">
+                    <ul>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-eye"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-star"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-shopping-cart"></i>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                  <img src="/images/women-01.jpg" alt="" />
+                </div>
+                <div className="down-content">
+                  <h4>Classic Spring</h4>
+                  <span>$120.00</span>
+                  <ul className="stars">
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </Slider>
+          </div>
+        </div>
+      </section>
+      <section className="section" id="kids">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="section-heading">
+                <h2>Kid's Latest</h2>
+                <span>
+                  Details to details is what makes Hexashop different from the other themes.
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="slick-container">
+          <div className="row">
+            <Slider {...settings} className="owl-carousel owl-loaded">
+              <div className="item">
+                <div className="thumb">
+                  <div className="hover-content">
+                    <ul>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-eye"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-star"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-shopping-cart"></i>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                  <img src="/images/kid-01.jpg" alt="" />
+                </div>
+                <div className="down-content">
+                  <h4>School Collection</h4>
+                  <span>$80.00</span>
+                  <ul className="stars">
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="item">
+                <div className="thumb">
+                  <div className="hover-content">
+                    <ul>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-eye"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-star"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-shopping-cart"></i>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                  <img src="/images/kid-02.jpg" alt="" />
+                </div>
+                <div className="down-content">
+                  <h4>Summer Cap</h4>
+                  <span>$12.00</span>
+                  <ul className="stars">
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="item">
+                <div className="thumb">
+                  <div className="hover-content">
+                    <ul>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-eye"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-star"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-shopping-cart"></i>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                  <img src="/images/kid-03.jpg" alt="" />
+                </div>
+                <div className="down-content">
+                  <h4>Classic Kid</h4>
+                  <span>$30.00</span>
+                  <ul className="stars">
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="item">
+                <div className="thumb">
+                  <div className="hover-content">
+                    <ul>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-eye"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-star"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/">
+                          <i className="fa fa-shopping-cart"></i>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                  <img src="/images/kid-01.jpg" alt="" />
+                </div>
+                <div className="down-content">
+                  <h4>Classic Spring</h4>
+                  <span>$120.00</span>
+                  <ul className="stars">
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fa fa-star"></i>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </Slider>
+          </div>
+        </div>
+      </section>
+      <div className="subscribe">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8">
+              <div className="section-heading">
+                <h2>By Subscribing To Our Newsletter You Can Get 30% Off</h2>
+                <span>
+                  Details to details is what makes Hexashop different from the other themes.
+                </span>
+              </div>
+              <form id="subscribe" action="" method="get">
+                <div className="row">
+                  <div className="col-lg-5">
+                    <fieldset>
+                      <input name="name" type="text" id="name" placeholder="Your Name" required />
+                    </fieldset>
+                  </div>
+                  <div className="col-lg-5">
+                    <fieldset>
+                      <input
+                        name="email"
+                        type="text"
+                        id="email"
+                        pattern="[^ @]*@[^ @]*"
+                        placeholder="Your Email Address"
+                        required
+                      />
+                    </fieldset>
+                  </div>
+                  <div className="col-lg-2">
+                    <fieldset>
+                      <button type="submit" id="form-submit" className="main-dark-button">
+                        <i className="fa fa-paper-plane"></i>
+                      </button>
+                    </fieldset>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="col-lg-4">
+              <div className="row">
+                <div className="col-6">
+                  <ul>
+                    <li>
+                      Store Location:
+                      <br />
+                      <span>Sunny Isles Beach, FL 33160, United States</span>
+                    </li>
+                    <li>
+                      Phone:
+                      <br />
+                      <span>010-6567-5303</span>
+                    </li>
+                    <li>
+                      Office Location:
+                      <br />
+                      <span>Silim</span>
+                    </li>
+                  </ul>
+                </div>
+                <div className="col-6">
+                  <ul>
+                    <li>
+                      Work Hours:
+                      <br />
+                      <span>07:30 AM - 9:30 PM Daily</span>
+                    </li>
+                    <li>
+                      Email:
+                      <br />
+                      <span>inajung7008@gmail.com</span>
+                    </li>
+                    <li>
+                      Social Media:
+                      <br />
+                      <span>
+                        <Link href="#">Facebook</Link>, <a href="#">Instagram</a>
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
