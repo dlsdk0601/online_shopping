01512889f260500da4d1868b8ed1159a840488a4
dlsdk0601 <dlsdk0601@gmail.com>
2023-05-29 02:20:11 +0900

    refactor: deps 파라미터로 받을 수 있게 수정

--- front/src/hooks/useScript.ts
+++ front/src/hooks/useScript.ts
@@ -1,21 +1,24 @@
 import { useEffect } from "react";
 import { isNotNil } from "../ex/utils";
 
-const useScript = (src: string, onLoad?: () => void) => {
-  useEffect(() => {
-    const script = document.createElement("script");
+const useScript = (src: string, onLoad?: () => void, deps?: any[]) => {
+  useEffect(
+    () => {
+      const script = document.createElement("script");
 
-    script.src = src;
-    document.head.appendChild(script);
-    script.onload = () => {
-      if (isNotNil(onLoad)) {
-        onLoad();
-      }
-    };
+      script.src = src;
+      document.head.appendChild(script);
+      script.onload = () => {
+        if (isNotNil(onLoad)) {
+          onLoad();
+        }
+      };
 
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, []);
+      return () => {
+        document.head.removeChild(script);
+      };
+    },
+    deps ? [...deps] : [],
+  );
 };
 export default useScript;
