d7205450c59ddbfb756827d1e3a5717de5f71bfb
dlsdk0601 <dlsdk0601@gmail.com>
2023-05-29 23:49:52 +0900

    refactor: PageUrl class 주석 풀고 TODO 추가

--- admin/src/url/url.ts
+++ admin/src/url/url.ts
@@ -1,32 +1,14 @@
-export {};
-// import { UrlObject } from "url";
-// import { encodeQuery, Parsed, QueryDefinition } from "../ex/query";
-//
-// export class PageUrl {
-//   readonly pathname: string;
-//
-//   constructor(pathname: string) {
-//     this.pathname = pathname;
-//   }
-//
-//   url(): UrlObject {
-//     return { pathname: this.pathname };
-//   }
-// }
-//
-// export class PageQueryUrl<T extends QueryDefinition<{}>> {
-//   readonly pathname: string;
-//   readonly query: T;
-//
-//   constructor(pathname: string, query: T) {
-//     this.pathname = pathname;
-//     this.query = query;
-//   }
-//
-//   url(queryParams: Parsed<T>): UrlObject {
-//     return {
-//       pathname: this.pathname,
-//       query: encodeQuery(this.query, queryParams),
-//     };
-//   }
-// }
+import { UrlObject } from "url";
+
+export class PageUrl {
+  readonly pathname: string;
+
+  constructor(pathname: string) {
+    this.pathname = pathname;
+  }
+
+  // TODO :: query 파라미터로 받기
+  url(): UrlObject {
+    return { pathname: this.pathname };
+  }
+}
