ad29919ed2fd544eb0cd4ecc6a452a3194d63cfc
dlsdk0601 <dlsdk0601@gmail.com>
2023-05-29 23:29:08 +0900

    refactor: 별점 컴포넌트 생성

---
+++ front/src/layout/components/Stars.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { range } from "lodash";
+
+const Stars = (props: { starScore: number; className?: string }) => {
+  const [starList, setStarList] = useState<number[]>([]);
+
+  useEffect(() => {
+    const newArray = range(1, props.starScore + 1);
+    setStarList(() => [...newArray]);
+  }, [props.starScore]);
+
+  return (
+    <ul className={`stars ${props.className && props.className}`}>
+      {starList.map((_, index) => (
+        <li key={`star-${index}`}>
+          <i className="fa fa-star"></i>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Stars;
