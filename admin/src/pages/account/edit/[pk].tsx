ef3a8b4b99870e0385b175ed3a0a81b4ae494d38
dlsdk0601 <dlsdk0601@gmail.com>
2023-05-31 01:54:43 +0900

    refactor: show user 페이지 재생성

--- admin/src/pages/account/edit/[pk].tsx
+++ admin/src/pages/account/edit/[pk].tsx
@@ -1,16 +1,47 @@
+import { Moment } from "moment";
+import { useQuery } from "react-query";
+import { useRouter } from "next/router";
+import { isNil } from "lodash";
+import CardSettings from "../../../components/tailwindEx/CardSettings";
+import { MomentFieldView, TextFieldView } from "../../../components/field/field";
+import useValueField from "../../../hooks/useValueField";
+import { queryKeys } from "../../../lib/contants";
+import { api } from "../../../api/url.g";
+import { isNotNil } from "../../../ex/utils";
+
 const UserShowPage = () => {
+  const router = useRouter();
+  const [id, setId] = useValueField("");
+  const [name, setName] = useValueField("");
+  const [phone, setPhone] = useValueField("");
+  const [createAt, setCreateAt] = useValueField<Moment | null>(null);
+
+  const pk = typeof router.query?.pk === "string" ? Number(router.query.pk) : undefined;
+
+  if (isNil(pk)) {
+    return <></>;
+  }
+
+  const { data: user } = useQuery([queryKeys.showUser, pk], () => api.showUser({ pk }), {
+    enabled: router.isReady && isNotNil(pk),
+  });
+
+  console.log(user);
+
   return (
     <>
       <div className="w-full px-4">
-        {/* <CardSettings> */}
-        {/*  <TextFieldView value={model.id} isShowingLabel /> */}
-        {/*  <TextFieldView value={model.name} isShowingLabel /> */}
-        {/*  <TextFieldView value={model.phone} isShowingLabel /> */}
-        {/*  <MomentFieldView value={model.createdAt} isShowingLabel /> */}
-        {/*  <button type="button" onClick={() => model.edit()}> */}
-        {/*    edit */}
-        {/*  </button> */}
-        {/* </CardSettings> */}
+        <CardSettings>
+          <TextFieldView value={id} onChange={(e) => setId.set(e.target.value)} label="아이디" />
+          <TextFieldView value={name} label="이름" onChange={(e) => setName.set(e.target.value)} />
+          <TextFieldView
+            value={phone}
+            label="휴대폰"
+            onChange={(e) => setPhone.set(e.target.value)}
+          />
+          <MomentFieldView value={createAt.value} label="생성 일자" />
+          <button type="button">edit</button>
+        </CardSettings>
       </div>
     </>
   );
