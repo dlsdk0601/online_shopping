53b8e7b0afa464c55593ab0d139d336d848015cd
dlsdk0601 <dlsdk0601@gmail.com>
2023-06-01 23:25:59 +0900

    refactor: 유저 리스트 페이지 api fetch 함수 => useQuery 로 수정

--- admin/src/pages/account/index.tsx
+++ admin/src/pages/account/index.tsx
@@ -1,7 +1,8 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import Router, { useRouter } from "next/router";
-import { isArray, isNil } from "lodash";
-import { UserListRes, UserListResUser } from "../../api/type.g";
+import { isArray } from "lodash";
+import { useQuery } from "react-query";
+import { UserListResUser } from "../../api/type.g";
 import { UserSearchType } from "../../api/enum.g";
 import { api } from "../../api/url.g";
 import SearchBarView from "../../components/table/searchBarView";
@@ -9,27 +10,13 @@ import { PaginationTableView } from "../../components/table/Table";
 import { Urls } from "../../url/url.g";
 import { d2 } from "../../ex/dateEx";
 import useIsReady from "../../hooks/useIsReady";
+import { queryKeys } from "../../lib/contants";
 
 const UserListPage = () => {
   const router = useRouter();
   const [page, setPage] = useState(1);
   const [search, setSearch] = useState("");
   const [searchType, setSearchType] = useState<UserSearchType | null>(null);
-  const [paginationUserList, setPaginationUserList] = useState<UserListRes | null>(null);
-
-  const onInit = useCallback(async () => {
-    const res = await api.userList({
-      page,
-      search,
-      searchType,
-    });
-
-    if (isNil(res)) {
-      return;
-    }
-
-    setPaginationUserList({ ...res });
-  }, [page, search, searchType]);
 
   const onSearch = useCallback(() => {
     const query = {
@@ -39,7 +26,7 @@ const UserListPage = () => {
     };
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const ignore = router.push({ ...router, query });
+    const ignore = router.push({ pathname: router.pathname, query });
   }, [page, search, searchType]);
 
   const enumToLabel = useCallback(
@@ -67,15 +54,17 @@ const UserListPage = () => {
     setSearchType(enumToLabel(searchType) ?? null);
   });
 
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const ignore = onInit();
-  }, [page, search, searchType]);
+  const { data: paginationUserList } = useQuery(
+    [queryKeys.userList, page],
+    () => api.userList({ page, search, searchType }),
+    {
+      keepPreviousData: true,
+    },
+  );
 
   return (
     <div className="mt-4">
       <div className="mb-12 w-full rounded px-4">
-        {/* TODO :: 검색 바 만들기 */}
         <SearchBarView<UserSearchType>
           onSubmit={() => onSearch()}
           searchType={searchType}
@@ -89,7 +78,7 @@ const UserListPage = () => {
           ]}
         />
         <PaginationTableView<UserListResUser>
-          pagination={paginationUserList}
+          pagination={paginationUserList ?? null}
           links={(paginationUserList?.rows ?? []).map(
             (item) => () =>
               Router.push({ pathname: Urls.account.edit["[pk]"], query: { pk: item.pk } }),
