e6ac6dfe6c1e4155a7908d503a1330346f01ca28
dlsdk0601 <dlsdk0601@gmail.com>
2023-06-02 22:57:10 +0900

    refactor: layout과 비즈니스 로직 컴포넌트 분리

--- admin/src/pages/account/edit/[pk].tsx
+++ admin/src/pages/account/edit/[pk].tsx
@@ -2,61 +2,65 @@ import moment, { Moment } from "moment";
 import { useQuery } from "react-query";
 import { useRouter } from "next/router";
 import { isNil } from "lodash";
+import React, { useEffect } from "react";
 import CardSettings from "../../../components/tailwindEx/CardSettings";
 import { MomentFieldView, TextFieldView } from "../../../components/field/field";
 import useValueField from "../../../hooks/useValueField";
 import { queryKeys } from "../../../lib/contants";
 import { api } from "../../../api/url.g";
 import { isNotNil } from "../../../ex/utils";
-import { Urls } from "../../../url/url.g";
-import useIsReady from "../../../hooks/useIsReady";
+import { ShowUserRes } from "../../../api/type.g";
 
 const UserShowPage = () => {
   const router = useRouter();
-  const [id, setId] = useValueField("");
-  const [name, setName] = useValueField("");
-  const [phone, setPhone] = useValueField("");
-  const [createAt, setCreateAt] = useValueField<Moment | null>(null);
-
   const pk = typeof router.query?.pk === "string" ? Number(router.query.pk) : undefined;
 
   if (isNil(pk)) {
-    // pk 가 제대로 되어있지 않다면 뒤로 보낸다.
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    return router.replace(Urls.account.index);
+    return <></>;
   }
 
   const { data: user } = useQuery([queryKeys.showUser, pk], () => api.showUser({ pk }), {
     enabled: router.isReady && isNotNil(pk),
   });
 
-  useIsReady(() => {
-    if (isNil(user)) {
+  return (
+    <div className="w-full px-4">
+      <UserShowView user={user} />
+    </div>
+  );
+};
+
+const UserShowView = React.memo((props: { user: ShowUserRes | undefined }) => {
+  const [id, setId] = useValueField("");
+  const [name, setName] = useValueField("");
+  const [phone, setPhone] = useValueField("");
+  const [createAt, setCreateAt] = useValueField<Moment | null>(null);
+
+  useEffect(() => {
+    if (isNil(props.user)) {
       return;
     }
 
-    setCreateAt.set(moment(user.create_at));
-    setName.set(user.name);
-    setPhone.set(user.phone);
-  }, [user]);
+    setCreateAt.set(moment(props.user.create_at));
+    setName.set(props.user.name);
+    setPhone.set(props.user.phone);
+  }, [props]);
 
   return (
     <>
-      <div className="w-full px-4">
-        <CardSettings>
-          <TextFieldView value={id} onChange={(e) => setId.set(e.target.value)} label="아이디" />
-          <TextFieldView value={name} label="이름" onChange={(e) => setName.set(e.target.value)} />
-          <TextFieldView
-            value={phone}
-            label="휴대폰"
-            onChange={(e) => setPhone.set(e.target.value)}
-          />
-          <MomentFieldView value={createAt.value} label="생성 일자" />
-          <button type="button">edit</button>
-        </CardSettings>
-      </div>
+      <CardSettings>
+        <TextFieldView value={id} onChange={(e) => setId.set(e.target.value)} label="아이디" />
+        <TextFieldView value={name} label="이름" onChange={(e) => setName.set(e.target.value)} />
+        <TextFieldView
+          value={phone}
+          label="휴대폰"
+          onChange={(e) => setPhone.set(e.target.value)}
+        />
+        <MomentFieldView value={createAt.value} label="생성 일자" />
+        <button type="button">edit</button>
+      </CardSettings>
     </>
   );
-};
+});
 
 export default UserShowPage;
