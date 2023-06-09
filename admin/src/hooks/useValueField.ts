9cd6b608e443263ed69b9cab66f101f478a6ee25
dlsdk0601 <dlsdk0601@gmail.com>
2023-06-10 02:37:37 +0900

    refactor: front 에서 수정한 useValueField 적용

--- admin/src/hooks/useValueField.ts
+++ admin/src/hooks/useValueField.ts
@@ -1,23 +1,59 @@
 import { useState } from "react";
+import { isEmpty, isNil } from "lodash";
 import { ValueField } from "../ex/field";
+import { isBlank, isNotNil } from "../ex/utils";
+import { k } from "../ex/korean-postposition";
 
-type onChangeStateParameter<U> = U | ((prev: U) => U);
-const useValueField = <T>(init: T) => {
-  const [state, setState] = useState<ValueField<T>>({ value: init, error: "" });
+type Validator = (value: string) => string | undefined;
+const useValueField = <T>(
+  init: T,
+  name: string,
+  ...validator: Validator[]
+): [
+  ValueField<T>,
+  { set: (payload: T) => void; err: (error?: string) => void; validate: () => boolean },
+] => {
+  const [state, setState] = useState<ValueField<T>>({ value: init, error: "", name });
 
-  const onChangeState = (action: onChangeStateParameter<T>) => {
-    if (typeof action === "function") {
-      setState((prev) => action(prev));
-    } else {
-      setState({ ...state, value: action });
-    }
+  const onChangeState = (payload: T) => {
+    setState({ ...state, value: payload, error: "" });
   };
 
-  const onError = (error: string) => {
+  const onError = (error?: string) => {
+    if (isNil(error)) {
+      onDefaultError();
+      return;
+    }
     setState({ ...state, error });
   };
 
-  return [state, { set: onChangeState, err: onError }];
+  const onDefaultError = () => {
+    setState({ ...state, error: k(`${state.name}(은|는) 필수입니다.`) });
+  };
+
+  const validate = (): boolean => {
+    let res = false;
+
+    if (isEmpty(validator) && isBlank(state.value)) {
+      onDefaultError();
+      res = true;
+      return res;
+    }
+
+    for (let i = 0; i < validator.length; i++) {
+      const val = validator[i];
+      const errorMessage = val(state.value);
+      if (isNotNil(errorMessage)) {
+        setState({ ...state, error: errorMessage });
+        res = true;
+        break;
+      }
+    }
+
+    return res;
+  };
+
+  return [state, { set: onChangeState, err: onError, validate }];
 };
 
 export default useValueField;
