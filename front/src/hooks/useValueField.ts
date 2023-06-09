c18721c8a7e4fff20672b65affc5c0d9af487154
dlsdk0601 <dlsdk0601@gmail.com>
2023-06-10 02:11:17 +0900

    refactor: useValueField 에 validate 추가

--- front/src/hooks/useValueField.ts
+++ front/src/hooks/useValueField.ts
@@ -1,12 +1,19 @@
 import { useState } from "react";
-import { isNil } from "lodash";
+import { isEmpty, isNil } from "lodash";
 import { ValueField } from "../ex/field";
 import { k } from "../ex/korean-postposition";
+import { isBlank, isNotNil } from "../ex/utils";
+
+type Validator = (value: string) => string | undefined;
 
 const useValueField = <T>(
   init: T,
   name: string,
-): [ValueField<T>, { set: (payload: T) => void; err: (error?: string) => void }] => {
+  ...validator: Validator[]
+): [
+  ValueField<T>,
+  { set: (payload: T) => void; err: (error?: string) => void; validate: () => boolean },
+] => {
   const [state, setState] = useState<ValueField<T>>({ value: init, error: "", name });
 
   const onChangeState = (payload: T) => {
@@ -25,7 +32,29 @@ const useValueField = <T>(
     setState({ ...state, error: k(`${state.name}(은|는) 필수입니다.`) });
   };
 
-  return [state, { set: onChangeState, err: onError }];
+  const validate = (): boolean => {
+    let res = false;
+
+    if (isEmpty(validator) && isBlank(state.value)) {
+      onDefaultError();
+      res = true;
+      return res;
+    }
+
+    for (let i = 0; i < validator.length; i++) {
+      const val = validator[i];
+      const errorMessage = val(state.value);
+      if (isNotNil(errorMessage)) {
+        setState({ ...state, error: errorMessage });
+        res = true;
+        break;
+      }
+    }
+
+    return res;
+  };
+
+  return [state, { set: onChangeState, err: onError, validate }];
 };
 
 export default useValueField;
