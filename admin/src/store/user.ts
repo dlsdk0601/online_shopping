ed4a41877cde683b9a495a889c059934f6fb5473
dlsdk0601 <dlsdk0601@gmail.com>
2023-06-10 15:02:10 +0900

    feat: token 모델 설정

--- admin/src/store/user.ts
+++ admin/src/store/user.ts
@@ -1,13 +1,33 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+import { isNil } from "lodash";
 import { ShowManagerRes } from "../api/type.g";
-import { queryKeys } from "../lib/contants";
+import { ATOM_KEY, CONSTANT } from "../lib/contants";
 
 export const userModel = atom<ShowManagerRes | null>({
-  key: queryKeys.user,
+  key: ATOM_KEY.USER,
   default: null,
 });
 
 export const userToken = atom<string | null>({
-  key: queryKeys.token,
+  key: ATOM_KEY.TOKEN,
   default: null,
+});
+
+export const tokenModel = selector<string | null>({
+  key: ATOM_KEY.TOKEN_MODEL,
+  get: ({ get }) => {
+    return get(userToken);
+  },
+  set: ({ set }, newValue) => {
+    if (typeof newValue === "string") {
+      set(userToken, newValue);
+      sessionStorage.setItem(CONSTANT.sessionTokenKey, newValue);
+      return;
+    }
+
+    if (isNil(newValue)) {
+      set(userToken, newValue);
+      sessionStorage.removeItem(CONSTANT.sessionTokenKey);
+    }
+  },
 });
