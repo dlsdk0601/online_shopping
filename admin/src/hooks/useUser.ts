ab53437ac18d601ff52b078a11aec19e776fc02f
dlsdk0601 <dlsdk0601@gmail.com>
2023-06-10 15:08:00 +0900

    refactor: 토큰도 지워주는 로직 추가

--- admin/src/hooks/useUser.ts
+++ admin/src/hooks/useUser.ts
@@ -5,7 +5,7 @@ import Router from "next/router";
 import { ShowManagerRes } from "../api/type.g";
 import { queryKeys } from "../lib/contants";
 import { api } from "../api/url.g";
-import { userModel } from "../store/user";
+import { tokenModel, userModel } from "../store/user";
 import { Urls } from "../url/url.g";
 import { ignorePromise } from "../ex/utils";
 
@@ -17,6 +17,7 @@ export interface UseUser {
 export function useUser(): UseUser {
   const queryClient = useQueryClient();
   const setUser = useSetRecoilState(userModel);
+  const setToken = useSetRecoilState(tokenModel);
 
   const { data: user } = useQuery(queryKeys.user, () => api.auth({}), {
     onSuccess: (received: ShowManagerRes | null) => {
@@ -30,6 +31,7 @@ export function useUser(): UseUser {
 
   const clearUser = () => {
     queryClient.setQueryData(queryKeys.user, null);
+    setToken(null);
     ignorePromise(() => Router.replace(Urls.auth["sign-in"].url()));
   };
 
