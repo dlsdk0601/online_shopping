945616c1cd4bba3bb54d709399b735b9cdf7fb0e
dlsdk0601 <dlsdk0601@gmail.com>
2023-06-10 15:03:05 +0900

    refactor: SignInView 반영

--- admin/src/pages/auth/sign-in.tsx
+++ admin/src/pages/auth/sign-in.tsx
@@ -1,40 +1,8 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { isNil } from "lodash";
-import { useRecoilState } from "recoil";
-import { useRouter } from "next/router";
+import React from "react";
 import SignFooterView from "../../components/layoutView/SignFooterView";
-import { api } from "../../api/url.g";
-import { userToken } from "../../store/user";
-import { Urls } from "../../url/url.g";
-import { ignorePromise, preventDefaulted, returnTo } from "../../ex/utils";
-import { CONSTANT } from "../../lib/contants";
+import SignInView from "../../view/SignInView";
 
 const SignInPage = () => {
-  const router = useRouter();
-  const [token, setToken] = useRecoilState(userToken);
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
-
-  useEffect(() => {
-    if (isNil(token)) {
-      return;
-    }
-
-    ignorePromise(() => router.replace(returnTo(router.query) ?? Urls.index.url()));
-  }, [token]);
-
-  const onSignIn = useCallback(async () => {
-    const res = await api.signIn({ id, password });
-
-    if (isNil(res)) {
-      return;
-    }
-
-    setToken(res.token);
-    sessionStorage.setItem(CONSTANT.sessionTokenKey, res.token);
-    ignorePromise(() => router.replace(Urls.index.url()));
-  }, [id, password]);
-
   return (
     <main>
       <section className="relative h-full min-h-screen w-full py-40">
@@ -49,41 +17,7 @@ const SignInPage = () => {
                   <hr className="border-b-1 mt-6 border-blueGray-300" />
                 </div>
                 <div className="flex-auto px-4 py-10 pt-0 lg:px-10">
-                  <form onSubmit={preventDefaulted(() => onSignIn())}>
-                    <div className="relative mb-3 w-full">
-                      <label className="mb-2 block text-xs font-bold uppercase text-blueGray-600">
-                        Id
-                      </label>
-                      <input
-                        type="text"
-                        className="w-full rounded border-0 bg-white px-3 py-3 text-sm text-blueGray-600 placeholder-blueGray-300 shadow transition-all duration-150 ease-linear focus:outline-none focus:ring"
-                        placeholder="id"
-                        value={id}
-                        onChange={(e) => setId(e.target.value)}
-                      />
-                    </div>
-
-                    <div className="relative mb-3 w-full">
-                      <label className="mb-2 block text-xs font-bold uppercase text-blueGray-600">
-                        Password
-                      </label>
-                      <input
-                        type="password"
-                        className="w-full rounded border-0 bg-white px-3 py-3 text-sm text-blueGray-600 placeholder-blueGray-300 shadow transition-all duration-150 ease-linear focus:outline-none focus:ring"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                      />
-                    </div>
-                    <div className="mt-6 text-center">
-                      <button
-                        type="submit"
-                        className="mr-1 mb-1 w-full rounded bg-blueGray-800 px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg"
-                      >
-                        Sign In
-                      </button>
-                    </div>
-                  </form>
+                  <SignInView />
                 </div>
               </div>
             </div>
