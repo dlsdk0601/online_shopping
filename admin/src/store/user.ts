5e253e520897ea73053a7043882f67b1cd791f37
dlsdk0601 <dlsdk0601@gmail.com>
2023-06-13 00:13:27 +0900

    refactor: user 정보 같은 경우 react-query 에 저장되므로 굳이 store 에 저장하지 않는다.

--- admin/src/store/user.ts
+++ admin/src/store/user.ts
@@ -1,12 +1,6 @@
 import { atom, selector } from "recoil";
 import { isNil } from "lodash";
-import { ShowManagerRes } from "../api/type.g";
 import { ATOM_KEY, CONSTANT } from "../lib/contants";
-
-export const userModel = atom<ShowManagerRes | null>({
-  key: ATOM_KEY.USER,
-  default: null,
-});
 
 export const userToken = atom<string | null>({
   key: ATOM_KEY.TOKEN,
