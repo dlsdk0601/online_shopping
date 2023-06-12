28ece41e0b7723a8561009edfea52419c92333b0
dlsdk0601 <dlsdk0601@gmail.com>
2023-06-13 00:13:58 +0900

    feat: 구독 이메일 보내는 페이지 생성

---
+++ admin/src/pages/subscribe/send-email.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CardSettings from "../../components/tailwindEx/CardSettings";
+
+const SendEmailPage = () => {
+  return (
+    <div className="w-full px-4">
+      <CardSettings>
+        <div>test</div>
+      </CardSettings>
+    </div>
+  );
+};
+
+export default SendEmailPage;
