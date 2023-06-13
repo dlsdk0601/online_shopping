c4322b1c8d769b088443de7d732d7d56456bc2b9
dlsdk0601 <dlsdk0601@gmail.com>
2023-06-13 22:22:55 +0900

    refactor: props.title 받는걸로 수정

--- admin/src/components/tailwindEx/CardSettings.tsx
+++ admin/src/components/tailwindEx/CardSettings.tsx
@@ -1,13 +1,13 @@
 import React, { PropsWithChildren } from "react";
 
-export default function CardSettings(props: PropsWithChildren) {
+export default function CardSettings(props: PropsWithChildren<{ title: string }>) {
   return (
     <>
       <div className="relative mb-6 flex min-h-screen-75 w-full min-w-0 flex-col break-words rounded-lg border-0 bg-blueGray-100 shadow-lg">
         <div className="flex-auto px-4 py-10 pt-0 lg:px-10">
           <form>
             <h6 className="mt-3 mb-6 text-sm font-bold uppercase text-blueGray-400">
-              User Information
+              {props.title}
             </h6>
             <div className="flex flex-wrap px-4">{props.children}</div>
           </form>
