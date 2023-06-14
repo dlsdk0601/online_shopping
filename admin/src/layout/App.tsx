093b2d447dc61a2ba338f203500a802cd3e22181
dlsdk0601 <dlsdk0601@gmail.com>
2023-06-14 22:56:14 +0900

    refactor: 불필요한 rq 옵션 주석, userApp 리펙토링

--- admin/src/layout/App.tsx
+++ admin/src/layout/App.tsx
@@ -9,8 +9,8 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import { ignorePromise } from "../ex/utils";
 import { DefaultLayoutView, LayoutView } from "./Layout";
 import { Urls } from "../url/url.g";
-import { tokenModel } from "../store/user";
 import { CONSTANT } from "../lib/contants";
+import { accessToken } from "../store/user";
 
 export const App = ({ Component, pageProps }: AppProps) => {
   const queryClient = new QueryClient({
@@ -19,10 +19,9 @@ export const App = ({ Component, pageProps }: AppProps) => {
         onError: () => Router.replace("/_error"),
         staleTime: 600000, // 10min
         cacheTime: 900000, // 15min, 데이터 유효 시간이 캐싱 저장 시간보다 길다는건 말이 안됨. 데이터 유효 시간을 기준으로 데이터를 다시 불러올텐데, 유효 시간이 더 길면 데이터가 없는 상태가 됨
-        refetchOnMount: false, // 데이터가 stale 상태일 경우 마운트 될때마다 refetch를 실행하는 옵션,
-        refetchOnWindowFocus: false, // 데이터가 stale 상태일 경우, 윈도우 포커싱될때마다 refetch하는 옵션
-        refetchOnReconnect: false, // 데이터가 stale 상태일 경우, 재 연결이 될때 refetch하는 옵션
-        // TODO :: 사실 위와 같은 option은 좋지 않다. 유저가 제대로 된 데이터를 접하지 못할 수도 있기 떄문에
+        // refetchOnMount: false, // 데이터가 stale 상태일 경우 마운트 될때마다 refetch를 실행하는 옵션,
+        // refetchOnWindowFocus: false, // 데이터가 stale 상태일 경우, 윈도우 포커싱될때마다 refetch하는 옵션
+        // refetchOnReconnect: false, // 데이터가 stale 상태일 경우, 재 연결이 될때 refetch하는 옵션
       },
       mutations: {
         onError: () => Router.replace("/_error"),
@@ -43,16 +42,6 @@ export const App = ({ Component, pageProps }: AppProps) => {
 
 const LayoutSelector = (props: PropsWithChildren) => {
   const router = useRouter();
-  const accessToken = sessionStorage.getItem(CONSTANT.sessionTokenKey);
-  const setToken = useSetRecoilState(tokenModel);
-
-  useEffect(() => {
-    if (isNil(accessToken)) {
-      return;
-    }
-
-    setToken(accessToken);
-  }, []);
 
   // 예외 URL
   const isSpecialUrl = some(["/_"], (prefix) => router.pathname.startsWith(prefix));
@@ -71,14 +60,19 @@ const LayoutSelector = (props: PropsWithChildren) => {
 
 const UserApp = (props: PropsWithChildren<Record<never, any>>) => {
   const router = useRouter();
-  const accessToken = sessionStorage.getItem(CONSTANT.sessionTokenKey);
+  const token = sessionStorage.getItem(CONSTANT.sessionTokenKey) ?? null;
+  const setToken = useSetRecoilState(accessToken);
+
+  useEffect(() => {
+    setToken(token);
+  }, []);
 
   if (!router.isReady) {
     return <DefaultLayoutView />;
   }
 
   // 로그인 전
-  if (isNil(accessToken)) {
+  if (isNil(token)) {
     return <Replace url={Urls.auth["sign-in"].url({ returnTo: router.asPath })} />;
   }
 
