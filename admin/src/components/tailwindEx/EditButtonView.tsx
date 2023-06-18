11c81956d999477c0b66a8b810f94acd631e4148
dlsdk0601 <dlsdk0601@gmail.com>
2023-06-19 01:17:29 +0900

    feat: createButtonView 생성

--- admin/src/components/tailwindEx/EditButtonView.tsx
+++ admin/src/components/tailwindEx/EditButtonView.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 
-const EditButtonView = (props: { isNew: boolean; onClick: () => void }) => {
+export const CreateButtonView = (props: { onClick: () => void }) => {
+  return (
+    <div className="mt-3 flex w-full justify-end">
+      <button
+        type="button"
+        className="rounded-lg bg-blue-400 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        onClick={() => props.onClick()}
+      >
+        생성
+      </button>
+    </div>
+  );
+};
+
+export const EditButtonView = (props: { isNew: boolean; onClick: () => void }) => {
   return (
     <div className="mt-3 flex w-full justify-end">
       <button
@@ -13,5 +27,3 @@ const EditButtonView = (props: { isNew: boolean; onClick: () => void }) => {
     </div>
   );
 };
-
-export default EditButtonView;
