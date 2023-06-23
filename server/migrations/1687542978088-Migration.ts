0d4ac39c6b4b891227980a9478f23c2641109c87
dlsdk0601 <dlsdk0601@gmail.com>
2023-06-24 03:04:18 +0900

    refactor: date 컬럼을 타임 스탬프로 찍는다.

---
+++ server/migrations/1687542978088-Migration.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migration1687542978088 implements MigrationInterface {
+    name = 'Migration1687542978088'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "subscribe_history" DROP COLUMN "send_time"`);
+        await queryRunner.query(`ALTER TABLE "subscribe_history" ADD "send_time" TIMESTAMP WITH TIME ZONE NOT NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "subscribe_history"."send_time" IS '발송 시간'`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`COMMENT ON COLUMN "subscribe_history"."send_time" IS '발송 시간'`);
+        await queryRunner.query(`ALTER TABLE "subscribe_history" DROP COLUMN "send_time"`);
+        await queryRunner.query(`ALTER TABLE "subscribe_history" ADD "send_time" date NOT NULL`);
+    }
+
+}
