bc9816f5f7e887a54f0422b6dc06d4ab77ea8d0c
dlsdk0601 <dlsdk0601@gmail.com>
2023-06-24 15:20:16 +0900

    refactor: 삭제 버튼 조건부 렌더링 추가

--- admin/src/components/tailwindEx/EditButtonView.tsx
+++ admin/src/components/tailwindEx/EditButtonView.tsx
@@ -14,12 +14,25 @@ export const CreateButtonView = (props: { onClick: () => void }) => {
   );
 };
 
-export const EditButtonView = (props: { isNew: boolean; onClick: () => void }) => {
+export const EditButtonView = (props: {
+  isNew: boolean;
+  onClick: () => void;
+  onDelete?: () => void;
+}) => {
   return (
     <div className="mt-3 flex w-full justify-end">
+      {props.onDelete && (
+        <button
+          type="button"
+          className="mr-3 rounded-lg bg-red-400 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
+          onClick={props.onDelete}
+        >
+          삭제
+        </button>
+      )}
       <button
         type="button"
-        className="rounded-lg bg-blue-400 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="rounded-lg bg-blue-400 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
         onClick={() => props.onClick()}
       >
         {props.isNew ? "저장" : "수정"}
