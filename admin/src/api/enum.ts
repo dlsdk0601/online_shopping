01c2b78ac5dc16cb150ae2393c816c2cedc73e1e
dlsdk0601 <dlsdk0601@gmail.com>
2023-06-25 22:23:35 +0900

    feat: category enum 모듈 생성

--- admin/src/api/enum.ts
+++ admin/src/api/enum.ts
@@ -1,4 +1,9 @@
-import { SubscribeHistorySearchType, SubscribeSearchType, UserSearchType } from "./enum.g";
+import {
+  ProductCategory,
+  SubscribeHistorySearchType,
+  SubscribeSearchType,
+  UserSearchType,
+} from "./enum.g";
 
 export const userSearchTypeEnumToLabel = (type: string | undefined): UserSearchType | undefined => {
   switch (type) {
@@ -31,5 +36,32 @@ export const subscribeHistorySearchTypeEnumToLabel = (
     case "ISSEND":
       return SubscribeHistorySearchType.ISSEND;
     default:
+  }
+};
+
+export const labelToCategoryEnum = (label: string | undefined): ProductCategory | undefined => {
+  switch (label) {
+    case "MEN":
+      return ProductCategory.MEN;
+    case "WOMEN":
+      return ProductCategory.WOMEN;
+    case "KIDS":
+      return ProductCategory.KIDS;
+    case "ACCESSORY":
+      return ProductCategory.ACCESSORY;
+    default:
+  }
+};
+
+export const categoryEnumToLabel = (label: "MEN" | "WOMEN" | "KIDS" | "ACCESSORY"): string => {
+  switch (label) {
+    case ProductCategory.MEN:
+      return "남성";
+    case ProductCategory.WOMEN:
+      return "여성";
+    case ProductCategory.KIDS:
+      return "키즈";
+    default: // ProductCategory.ACCESSORY
+      return "악세서리";
   }
 };
