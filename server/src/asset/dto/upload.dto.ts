4c34d5ebf11f58d23f237dfdb53a2e4789810483
dlsdk0601 <dlsdk0601@gmail.com>
2023-06-29 01:06:48 +0900

    feat: multiple 업로드 API DTO 생성

--- server/src/asset/dto/upload.dto.ts
+++ server/src/asset/dto/upload.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty, getSchemaPath } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 import { FileSetDto } from "./fileSet.dto";
 
@@ -22,4 +22,22 @@ export class UploadReqDto {
 export class UploadResDto {
   @ApiProperty({ description: "파일 데이터 set", type: FileSetDto })
   fileSet: FileSetDto;
+}
+
+export class UploadsReqDto {
+  @ApiProperty({
+    description: "파일 base64 list",
+    type: "array",
+    items: { $ref: getSchemaPath(UploadReqDto) },
+  })
+  files: UploadReqDto[];
+}
+
+export class UploadsResDto {
+  @ApiProperty({
+    description: "파일 데이터 set list",
+    type: "array",
+    items: { $ref: getSchemaPath(FileSetDto) },
+  })
+  fileSets: FileSetDto[];
 }
