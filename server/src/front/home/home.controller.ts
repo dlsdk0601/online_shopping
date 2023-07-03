486b1e9fd53471be59f2eda9e01e73d689dbd913
dlsdk0601 <dlsdk0601@gmail.com>
2023-07-04 02:29:29 +0900

    feat: HomeApi service, controller 생성

--- server/src/front/home/home.controller.ts
+++ server/src/front/home/home.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { ApiCreatedResponse, ApiTags } from "@nestjs/swagger";
 import { HomeService } from "./home.service";
+import { HomeReqDto, HomeResDto } from "./dto/home.dto";
 
 @Controller("home")
+@ApiTags("메인 화면")
 export class HomeController {
   constructor(private readonly homeService: HomeService) {}
+
+  @Post("")
+  @ApiCreatedResponse({ type: HomeResDto })
+  async home(@Body() body: HomeReqDto) {
+    return this.homeService.home(body);
+  }
 }
--- server/src/front/home/home.service.ts
+++ server/src/front/home/home.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from "@nestjs/common";
+import { AssetService } from "../../asset/asset.service";
+import { HomeReqDto } from "./dto/home.dto";
 
 @Injectable()
-export class HomeService {}
+export class HomeService {
+  constructor(private assetService: AssetService) {}
+
+  async home(body: HomeReqDto) {}
+}
