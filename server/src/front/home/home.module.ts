2331e6a42e7229bc0a0c92186d0c3e6c028988fb
dlsdk0601 <dlsdk0601@gmail.com>
2023-07-04 02:28:51 +0900

    refactor: Product entity, AssetService 추가

--- server/src/front/home/home.module.ts
+++ server/src/front/home/home.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { HomeService } from './home.service';
-import { HomeController } from './home.controller';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { HomeService } from "./home.service";
+import { HomeController } from "./home.controller";
+import { Product } from "../../entities/product.entity";
+import { AssetService } from "../../asset/asset.service";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [HomeController],
-  providers: [HomeService]
+  providers: [HomeService, AssetService],
 })
 export class HomeModule {}
