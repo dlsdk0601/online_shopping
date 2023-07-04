9904c2071b5b4c115fa21d52e8a9ba2eb111e812
dlsdk0601 <dlsdk0601@gmail.com>
2023-07-05 05:51:07 +0900

    feat: product Mens 쿼리 생성

--- server/src/front/home/home.service.ts
+++ server/src/front/home/home.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from "@nestjs/common";
 import { AssetService } from "../../asset/asset.service";
 import { HomeReqDto } from "./dto/home.dto";
+import { Product } from "../../entities/product.entity";
+import { ProductCategory } from "../../type/commonType";
 
 @Injectable()
 export class HomeService {
   constructor(private assetService: AssetService) {}
 
-  async home(body: HomeReqDto) {}
+  async home(body: HomeReqDto) {
+    const productMens = await Product.find({
+      where: {
+        category: ProductCategory.MEN,
+      },
+      take: 3,
+      order: {
+        create_at: "asc",
+      },
+    });
+
+    console.log(productMens);
+  }
 }
