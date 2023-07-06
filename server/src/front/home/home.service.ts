1d650ae97c3fdc1d22a7e7e2c87947ff261e5946
dlsdk0601 <dlsdk0601@gmail.com>
2023-07-06 22:48:33 +0900

    feat: 메인화면 상품 리스트 추상화

--- server/src/front/home/home.service.ts
+++ server/src/front/home/home.service.ts
@@ -9,16 +9,29 @@ export class HomeService {
   constructor(private assetService: AssetService) {}
 
   async home(body: HomeReqDto) {
-    const productMens = await Product.find({
-      where: {
-        category: ProductCategory.MEN,
-      },
-      take: 3,
-      order: {
-        create_at: "asc",
+    const productMens = await this.prodictList(ProductCategory.MEN, 6);
+    const productWomens = await this.prodictList(ProductCategory.WOMEN, 6);
+    const productKids = await this.prodictList(ProductCategory.KIDS, 6);
+
+    return {
+      mainBanners: [],
+      mensLatestItems: productMens,
+      womensLatestItems: productWomens,
+      kidsLatestItems: productKids,
+    };
+  }
+
+  async prodictList(category: ProductCategory, limit: number) {
+    return Product.find({
+      where: { category },
+      take: limit,
+      order: { create_at: "desc" },
+      select: {
+        pk: true,
+        name: true,
+        price: true,
+        category: true,
       },
     });
-
-    console.log(productMens);
   }
 }
