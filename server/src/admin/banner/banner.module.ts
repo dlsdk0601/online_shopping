96f474a1c6a1cf44caad7dad943659511cec6f61
dlsdk0601 <dlsdk0601@gmail.com>
2023-07-06 23:26:27 +0900

    feat: typeorm 설정 및 assetService import 추가

--- server/src/admin/banner/banner.module.ts
+++ server/src/admin/banner/banner.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { BannerService } from './banner.service';
-import { BannerController } from './banner.controller';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { BannerService } from "./banner.service";
+import { BannerController } from "./banner.controller";
+import { AssetService } from "../../asset/asset.service";
+import { MainBanner } from "../../entities/main-banner.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MainBanner])],
   controllers: [BannerController],
-  providers: [BannerService]
+  providers: [BannerService, AssetService],
 })
 export class BannerModule {}
