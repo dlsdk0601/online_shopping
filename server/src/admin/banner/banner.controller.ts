1db2abd1c2b1a62174ced237c5bb90eecf08d2fc
dlsdk0601 <dlsdk0601@gmail.com>
2023-07-06 23:27:14 +0900

    feat: list, show, edit API 생성

--- server/src/admin/banner/banner.controller.ts
+++ server/src/admin/banner/banner.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { ApiCreatedResponse, ApiTags } from "@nestjs/swagger";
 import { BannerService } from "./banner.service";
+import {
+  BannerListReqDto,
+  BannerListResDto,
+  ShowBannerReqDto,
+  ShowBannerResDto,
+} from "./dto/show-banner.dto";
+import { EditBannerReqDto, EditBannerResDto } from "./dto/edit-banner.dto";
 
-@Controller("banner")
+@Controller("admin")
+@ApiTags("admin-banner")
 export class BannerController {
   constructor(private readonly bannerService: BannerService) {}
+
+  @Post("banner-list")
+  @ApiCreatedResponse({ type: BannerListResDto })
+  async list(@Body() body: BannerListReqDto) {
+    return this.bannerService.list();
+  }
+
+  @Post("show-banner")
+  @ApiCreatedResponse({ type: ShowBannerResDto })
+  async show(@Body() body: ShowBannerReqDto) {
+    return this.bannerService.show(body.pk);
+  }
+
+  @Post("edit-banner")
+  @ApiCreatedResponse({ type: EditBannerResDto })
+  async edit(@Body() body: EditBannerReqDto) {
+    return this.bannerService.edit(body);
+  }
 }
--- server/src/admin/banner/banner.service.ts
+++ server/src/admin/banner/banner.service.ts
@@ -1,4 +1,76 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException, NotFoundException } from "@nestjs/common";
+import { isNil } from "lodash";
+import { AssetService } from "../../asset/asset.service";
+import { MainBanner } from "../../entities/main-banner.entity";
+import errorMessage from "../../config/errorMessage";
+import { EditBannerReqDto } from "./dto/edit-banner.dto";
 
 @Injectable()
-export class BannerService {}
+export class BannerService {
+  constructor(private readonly assetService: AssetService) {}
+
+  async list() {
+    const banners = await MainBanner.find({
+      select: {
+        pk: true,
+        title: true,
+        sub_title: true,
+        description: true,
+        category: true,
+      },
+      relations: {
+        image: true,
+      },
+    });
+
+    const list = banners.map((banner) => {
+      return { ...banner, image: this.assetService.fromUuid(banner.image.uuid) };
+    });
+
+    return { list };
+  }
+
+  async show(pk: number) {
+    const banner = await MainBanner.findOne({ where: { pk }, relations: { image: true } });
+
+    if (isNil(banner)) {
+      throw new NotFoundException(errorMessage.NOT_FOUND_DATA);
+    }
+
+    return {
+      pk: banner.pk,
+      title: banner.title,
+      description: banner.description,
+      subTitle: banner.sub_title,
+      category: banner.category,
+      image: this.assetService.fromUuid(banner.image.uuid),
+    };
+  }
+
+  async edit(body: EditBannerReqDto) {
+    let banner: MainBanner | null;
+
+    if (isNil(body.pk)) {
+      banner = await MainBanner.findOne({ where: { pk: body.pk }, relations: { image: true } });
+    } else {
+      banner = new MainBanner();
+    }
+
+    if (isNil(banner)) {
+      throw new NotFoundException(errorMessage.NOT_FOUND_DATA);
+    }
+
+    banner.title = body.title;
+    banner.sub_title = body.subTitle;
+    banner.description = body.description;
+    banner.category = body.category;
+    banner.image = await this.assetService.fromUuid(body.image);
+
+    try {
+      await banner.save();
+      return { pk: banner.pk };
+    } catch (e) {
+      throw new InternalServerErrorException(errorMessage.INTERNAL_FAILED);
+    }
+  }
+}
