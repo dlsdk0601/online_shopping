effc07045392ffedfb72412433267c6414c2c735
dlsdk0601 <dlsdk0601@gmail.com>
2023-07-06 23:26:44 +0900

    feat: show DTO 추가

--- server/src/admin/banner/dto/show-banner.dto.ts
+++ server/src/admin/banner/dto/show-banner.dto.ts
@@ -1,6 +1,8 @@
 import { ApiExtraModels, ApiProperty, getSchemaPath } from "@nestjs/swagger";
 import { IsEnum, IsISO8601, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { Optional } from "@nestjs/common";
 import { ProductCategory } from "../../../type/commonType";
+import { FileSetDto } from "../../../asset/dto/fileSet.dto";
 
 export class BannerListReqDto {}
 
@@ -36,12 +38,45 @@ export class BannerListResDto {
   list: BannerListResBannerDto[];
 }
 
-// @ApiProperty({ description: "배너 설명", nullable: true, type: "string" })
-// @IsString()
-// @Optional()
-// description: string;
+export class ShowBannerReqDto {
+  @ApiProperty({ description: "pk", type: "number", nullable: false })
+  @IsNumber()
+  @IsNotEmpty()
+  pk: number;
+}
 
-// @ApiProperty({ description: "배너 서브 제목", nullable: false, type: "string" })
-// @IsString()
-// @IsNotEmpty()
-// subTitle: string;
+@ApiExtraModels(FileSetDto)
+export class ShowBannerResDto {
+  @ApiProperty({ description: "pk", type: "number", nullable: false })
+  @IsNumber()
+  @IsNotEmpty()
+  pk: number;
+
+  @ApiProperty({ description: "배너 제목", nullable: false, type: "string" })
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @ApiProperty({ description: "배너 설명", nullable: true, type: "string" })
+  @IsString()
+  @Optional()
+  description: string | null;
+
+  @ApiProperty({ description: "배너 서브 제목", nullable: false, type: "string" })
+  @IsString()
+  @IsNotEmpty()
+  subTitle: string;
+
+  @ApiProperty({ description: "카테고리", nullable: false, enum: ProductCategory })
+  @IsEnum(ProductCategory)
+  @IsNotEmpty()
+  category: ProductCategory;
+
+  @ApiProperty({
+    description: "이미지",
+    nullable: false,
+    items: { $ref: getSchemaPath(FileSetDto) },
+  })
+  @IsNotEmpty()
+  image: FileSetDto;
+}
