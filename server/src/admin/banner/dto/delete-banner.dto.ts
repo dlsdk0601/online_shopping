4ea626d53c9540927690bc4ae56f5892a34acf32
dlsdk0601 <dlsdk0601@gmail.com>
2023-07-09 21:47:48 +0900

    refactor: Transform 추가

--- server/src/admin/banner/dto/delete-banner.dto.ts
+++ server/src/admin/banner/dto/delete-banner.dto.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsNumber } from "class-validator";
+import { Transform } from "class-transformer";
 
 export class DeleteBannerReqDto {
   @ApiProperty({ description: "pk", type: "number", nullable: false })
+  @Transform(({ value }: { value: string }) => parseInt(value, 10)) // TODO :: 모듈화
   @IsNumber()
   @IsNotEmpty()
   pk: number;
