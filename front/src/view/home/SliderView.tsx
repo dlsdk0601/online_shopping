fe138640f1b61cc8201bf4456026fd4aff2730e5
dlsdk0601 <dlsdk0601@gmail.com>
2023-07-12 22:59:41 +0900

    feat: 메인 화면 슬라이더 컴포넌트 생성

---
+++ front/src/view/home/SliderView.tsx
@@ -0,0 +1,76 @@
+import Slider from "react-slick";
+import { ProductListItem } from "../../api/type.g";
+import { mf2 } from "../../ex/numberEx";
+
+const SliderView = (props: { items: ProductListItem[] }) => {
+  return (
+    <div className="row">
+      <Slider
+        className="owl-carousel owl-loaded"
+        dots={false}
+        infinite
+        speed={300}
+        slidesToShow={3}
+        slidesToScroll={1}
+        responsive={[
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 2,
+            },
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              arrows: false,
+            },
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+              arrows: false,
+            },
+          },
+        ]}
+      >
+        {props.items &&
+          props.items.map((item) => {
+            return (
+              <li className="item">
+                <div className="thumb">
+                  <div className="hover-content">
+                    <ul>
+                      <li>
+                        <a href="single-product.html">
+                          <i className="fa fa-eye" />
+                        </a>
+                      </li>
+                      <li>
+                        <a href="single-product.html">
+                          <i className="fa fa-star" />
+                        </a>
+                      </li>
+                      <li>
+                        <a href="single-product.html">
+                          <i className="fa fa-shopping-cart" />
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <img src={item.image.url} alt={`${item.category}-img-${item.pk}`} />
+                </div>
+                <div className="down-content">
+                  <h4>{item.name}</h4>
+                  <span>${mf2(item.price)}</span>
+                </div>
+              </li>
+            );
+          })}
+      </Slider>
+    </div>
+  );
+};
+
+export default SliderView;
