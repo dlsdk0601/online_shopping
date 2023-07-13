0e295f5ea457935a67dfcb424cf5d0fba05c0b9e
dlsdk0601 <dlsdk0601@gmail.com>
2023-07-14 01:56:01 +0900

    refactor: Get 방식이라서 body => query 로 수정

--- server/src/front/home/home.controller.ts
+++ server/src/front/home/home.controller.ts
@@ -1,16 +1,16 @@
-import { Body, Controller, Get } from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
 import { ApiCreatedResponse, ApiTags } from "@nestjs/swagger";
 import { HomeService } from "./home.service";
 import { HomeReqDto, HomeResDto } from "./dto/home.dto";
 
-@Controller("home")
+@Controller("")
 @ApiTags("메인 화면")
 export class HomeController {
   constructor(private readonly homeService: HomeService) {}
 
-  @Get("")
+  @Get("home")
   @ApiCreatedResponse({ type: HomeResDto })
-  async home(@Body() body: HomeReqDto) {
-    return this.homeService.home(body);
+  async home(@Query() query: HomeReqDto) {
+    return this.homeService.home(query);
   }
 }
