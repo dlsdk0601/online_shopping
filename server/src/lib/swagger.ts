ed2ed00338ceccc3aaad9480b9c51dca6dda82af
dlsdk0601 <dlsdk0601@gmail.com>
2023-07-15 01:15:52 +0900

    refactor: front, admin 문서화 추가

--- server/src/lib/swagger.ts
+++ server/src/lib/swagger.ts
@@ -1,9 +1,15 @@
 import { INestApplication } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { NestFactory } from "@nestjs/core";
 import { writeFileSync } from "fs";
 import constant from "../config/constant";
+import { FrontAppModule } from "../front/front.module";
+import { AdminAppModule } from "../admin/admin.module";
 
-export const setupSwagger = (app: INestApplication) => {
+export const setupSwagger = async (app: INestApplication) => {
+  const frontApp = await NestFactory.create(FrontAppModule);
+  const adminApp = await NestFactory.create(AdminAppModule);
+
   // Swagger options 설정
   const options = new DocumentBuilder()
     .setTitle("online-shopping")
@@ -24,6 +30,16 @@ export const setupSwagger = (app: INestApplication) => {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup("swagger", app, document);
   writeFileSync("./api.json", JSON.stringify(document), {
+    encoding: "utf-8",
+  });
+
+  const frontDocument = SwaggerModule.createDocument(frontApp, options);
+  writeFileSync("./front-api.json", JSON.stringify(frontDocument), {
+    encoding: "utf-8",
+  });
+
+  const adminDocument = SwaggerModule.createDocument(adminApp, options);
+  writeFileSync("./admin-api.json", JSON.stringify(adminDocument), {
     encoding: "utf-8",
   });
 };
