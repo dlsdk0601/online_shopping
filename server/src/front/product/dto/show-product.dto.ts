eff847a6e2cfcaf261c234e2422023319340b5e5
dlsdk0601 <dlsdk0601@gmail.com>
2023-07-19 23:47:53 +0900

    feat: product show DTO 생성

--- server/src/front/product/dto/show-product.dto.ts
+++ server/src/front/product/dto/show-product.dto.ts
@@ -1,10 +1,11 @@
 import { ApiExtraModels, ApiProperty, getSchemaPath } from "@nestjs/swagger";
 import { Transform } from "class-transformer";
-import { IsArray, IsEnum, IsNotEmpty, IsNumber } from "class-validator";
+import { IsArray, IsEnum, IsNotEmpty, IsNumber, IsString } from "class-validator";
 import { toNumber } from "src/lib/dtoTransformEx";
 import { ProductCategory } from "../../../type/commonType";
 import { ProductListItem } from "../../commonDto/ProductListItem.dto";
 import { PaginationDto } from "../../../type/pagination.dto";
+import { FileSetDto } from "../../../asset/dto/fileSet.dto";
 
 export class ProductListReqDto {
   @ApiProperty({ description: "page number", nullable: false, type: "number" })
@@ -35,4 +36,62 @@ export class ProductListResDto extends PaginationDto {
     super(count, page);
     this.rows = data;
   }
+}
+
+export class ShowProductReqDto {
+  @ApiProperty({ description: "상품 pk", nullable: false, type: "number" })
+  @IsNumber()
+  @IsNotEmpty()
+  pk: number;
+}
+
+export class ShowProductResDto {
+  @ApiProperty({ description: "상품 pk", nullable: false, type: "number" })
+  @IsNumber()
+  @IsNotEmpty()
+  pk: number;
+
+  @ApiProperty({ description: "상품 이름", nullable: false, type: "string" })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({ description: "상품 가격", nullable: false, type: "number" })
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
+
+  @ApiProperty({ description: "상품 재고", nullable: false, type: "number" })
+  @IsNumber()
+  @IsNotEmpty()
+  stockCount: number;
+
+  @ApiProperty({
+    description: "상품 카테고리",
+    nullable: false,
+    type: "enum",
+    enum: ProductCategory,
+  })
+  @IsEnum(ProductCategory)
+  @IsNotEmpty()
+  category: ProductCategory;
+
+  @ApiProperty({
+    description: "상품 메인 이미지",
+    nullable: false,
+    type: "array",
+    items: { $ref: getSchemaPath(FileSetDto) },
+  })
+  @IsNotEmpty()
+  subImages: FileSetDto[];
+
+  @ApiProperty({ description: "상품 설명 타이틀", nullable: false, type: "string" })
+  @IsString()
+  @IsNotEmpty()
+  descriptionTitle: string;
+
+  @ApiProperty({ description: "상품 설명", nullable: false, type: "string" })
+  @IsString()
+  @IsNotEmpty()
+  description: string;
 }
