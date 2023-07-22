70714cd416b5da35cadf40ca26a4c67aa01dc1b0
dlsdk0601 <dlsdk0601@gmail.com>
2023-07-22 14:26:41 +0900

    feat: cart module 추가

--- server/src/front/front.module.ts
+++ server/src/front/front.module.ts
@@ -8,6 +8,7 @@ import { AuthModule } from "./auth/auth.module";
 import { typeOrmModuleOptions } from "../database/ormconfig";
 import { envValidation } from "../lib/envValidation";
 import { AssetModule } from "../asset/asset.module";
+import { CartModule } from './cart/cart.module';
 
 // 전역 변수 설정 및 orm 설정을 front / admin 따로 해준다.
 // 다른 설정이 들어 갈 수도 있기 때문에
@@ -24,6 +25,7 @@ import { AssetModule } from "../asset/asset.module";
     ProductModule,
     HomeModule,
     AuthModule,
+    CartModule,
   ],
 })
 export class FrontAppModule {}
