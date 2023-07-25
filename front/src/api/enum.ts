ca2016ff6c69f8dfe5241559215c75fbc9fca806
dlsdk0601 <dlsdk0601@gmail.com>
2023-07-26 01:18:32 +0900

    refactor: default 를 LOCAL 로 수정

--- front/src/api/enum.ts
+++ front/src/api/enum.ts
@@ -1,17 +1,17 @@
 import { UserType } from "./enum.g";
 
-export const userTypeLabelToEnum = (str: string): UserType => {
+export const userTypeLabelToEnum = (str: string | null | undefined): UserType => {
   switch (str) {
     case "GOOGLE":
       return UserType.GOOGLE;
-    case "LOCAL":
-      return UserType.LOCAL;
     case "APPLE":
       return UserType.APPLE;
     case "KAKAO":
       return UserType.KAKAO;
     case "NAVER":
-    default:
       return UserType.NAVER;
+    case "LOCAL":
+    default:
+      return UserType.LOCAL;
   }
 };
