91e08cec3113ce6488e25cfe43ae0141cf99132b
dlsdk0601 <dlsdk0601@gmail.com>
2023-07-29 02:10:12 +0900

    feat: 장바구니 리스트 API 생성

--- server/src/front/cart/cart.controller.ts
+++ server/src/front/cart/cart.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { ApiCreatedResponse, ApiTags } from "@nestjs/swagger";
+import { AuthGuard } from "@nestjs/passport";
 import { CartService } from "./cart.service";
+import { CartListReqDto, CartListResDto } from "./dto/show-cart.dto";
+import { GetUser } from "../../decorator/user.decorator";
+import { GlobalUser } from "../../type/type";
 
-@Controller("cart")
+@Controller("/cart")
+@ApiTags("장바구니")
 export class CartController {
   constructor(private readonly cartService: CartService) {}
+
+  @Post("/cart-list")
+  @UseGuards(AuthGuard("jwt"))
+  @ApiCreatedResponse({ type: CartListResDto })
+  async list(@Body() body: CartListReqDto, @GetUser() user: GlobalUser) {
+    return this.cartService.list(body, user.pk);
+  }
 }
--- server/src/front/cart/cart.service.ts
+++ server/src/front/cart/cart.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, InternalServerErrorException } from "@nestjs/common";
+import { isNil } from "lodash";
+import { AssetService } from "../../asset/asset.service";
+import { CartListReqDto } from "./dto/show-cart.dto";
+import { User } from "../../entities/user.entity";
+import errorMessage from "../../config/errorMessage";
 
 @Injectable()
-export class CartService {}
+export class CartService {
+  constructor(private assetService: AssetService) {}
+
+  async list(body: CartListReqDto, pk: number) {
+    const userEntity = await User.findOne({
+      where: {
+        pk,
+      },
+      relations: {
+        cart: {
+          cart_products: {
+            product: {
+              main_image: true,
+            },
+          },
+        },
+      },
+    });
+
+    if (isNil(userEntity)) {
+      throw new BadRequestException(errorMessage.BAD_REQUEST);
+    }
+
+    if (userEntity.cart.length > 1) {
+      throw new InternalServerErrorException(errorMessage.INTERNAL_FAILED);
+    }
+
+    const cartEntity = userEntity.cart[0];
+
+    return {
+      list: cartEntity.cart_products.map((cart) => ({
+        pk: cart.pk,
+        name: cart.product.name,
+        price: cart.product.price,
+        count: cart.count,
+        image: cart.product.main_image,
+      })),
+    };
+  }
+}
