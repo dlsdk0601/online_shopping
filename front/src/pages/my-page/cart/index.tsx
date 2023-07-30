58478aaa59d2c5db6fbaea9d9ef79ab07864009a
dlsdk0601 <dlsdk0601@gmail.com>
2023-07-30 21:54:05 +0900

    feat: 장바구니 css 처리

--- front/src/pages/my-page/cart/index.tsx
+++ front/src/pages/my-page/cart/index.tsx
@@ -1,40 +1,66 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import { isEmpty, isNil } from "lodash";
+import { api } from "../../../api/url.g";
+import { queryKeys } from "../../../lib/contants";
+import { isNotNil } from "../../../ex/utils";
+import { mf1 } from "../../../ex/numberEx";
 
 const CartPage = () => {
   const router = useRouter();
+
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const { data: cartList } = useQuery([queryKeys.cart], () => api.cartList({}), {
+    onSuccess: (res) => {
+      if (isNil(res)) {
+        return;
+      }
+
+      let total = 0;
+      for (let i = 0; i < res.list.length; i++) {
+        total += (res.list[i]?.count ?? 0) * (res.list[i]?.price ?? 0);
+      }
+
+      setTotalPrice(total);
+    },
+  });
+
   return (
-    <>
-      <div className="col-lg-4">
-        <li className="item">{/*  item */}</li>
-      </div>
-      <div className="col-lg-12">
-        <div className="pagination">
-          <ul>
-            <li>
-              <Link href="/">-</Link>
-            </li>
-            {[1, 2, 3, 4].map((page) => {
-              return (
-                <li key={`product-women-list-page-${page}`}>
-                  <Link
-                    href={{
-                      pathname: router.pathname,
-                      query: { ...router.query, page },
-                    }}
-                  >
-                    {page}
-                  </Link>
-                </li>
-              );
-            })}
-            <li>
-              <Link href="/">+</Link>
-            </li>
-          </ul>
+    <section className="section mt-4" id="products">
+      <div className="container sign-container">
+        <div className="row">
+          <div className="cart-item-wrapper">
+            {(isNil(cartList) || isEmpty(cartList.list)) && <li>cart is empty</li>}
+            {isNotNil(cartList) &&
+              !isEmpty(cartList.list) &&
+              cartList.list.map((cart) => {
+                return (
+                  <li className="rounded">
+                    <figure className="figure">
+                      <img className="figure-img rounded" src={cart.image.url} alt="cart-img" />
+                    </figure>
+                    <p>{cart.name}</p>
+                    <div>
+                      <div>
+                        <button type="button" className="count-button">
+                          +
+                        </button>
+                        {cart.count}
+                        <button type="button" className="count-button">
+                          -
+                        </button>
+                      </div>
+                    </div>
+                    <p className="price">${mf1(cart.count * cart.price)}</p>
+                  </li>
+                );
+              })}
+          </div>
         </div>
       </div>
-    </>
+    </section>
   );
 };
 
