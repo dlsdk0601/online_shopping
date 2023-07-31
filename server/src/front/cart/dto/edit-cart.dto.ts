0bcef3d5fae730ae75cbaafacc25ee8a0fd65337
dlsdk0601 <dlsdk0601@gmail.com>
2023-08-01 00:22:22 +0900

    feat: 장바구니 카운트 edit dto 생성

--- server/src/front/cart/dto/edit-cart.dto.ts
+++ server/src/front/cart/dto/edit-cart.dto.ts
@@ -1,4 +1,27 @@
-import { PartialType } from "@nestjs/swagger";
-import { AddCartDto } from "./add-cart.dto";
+import { ApiExtraModels, ApiProperty, getSchemaPath } from "@nestjs/swagger";
+import { IsArray, IsNotEmpty, IsNumber } from "class-validator";
+import { CartListItemDto } from "./show-cart.dto";
 
-export class EditCartDto extends PartialType(AddCartDto) {}
+export class EditCartProductCountReqDto {
+  @ApiProperty({ description: "장바구니 상품 pk", nullable: false, type: "number" })
+  @IsNumber()
+  @IsNotEmpty()
+  pk: number;
+
+  @ApiProperty({ description: "상품 수량", nullable: false, type: "number" })
+  @IsNumber()
+  @IsNotEmpty()
+  count: number;
+}
+
+@ApiExtraModels(CartListItemDto)
+export class EditCartProductCountResDto {
+  @ApiProperty({
+    description: "장바구니 상품 데이터",
+    nullable: false,
+    items: { $ref: getSchemaPath(CartListItemDto) },
+  })
+  @IsArray()
+  @IsNotEmpty()
+  data: CartListItemDto;
+}
