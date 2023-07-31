bf60b27d1fc77655ee7f7111cc47ce28e6da025b
dlsdk0601 <dlsdk0601@gmail.com>
2023-07-31 23:10:11 +0900

    refactor: 스켈레톤 및 data undefined 처리

--- front/src/pages/my-page/cart/index.tsx
+++ front/src/pages/my-page/cart/index.tsx
@@ -1,67 +1,36 @@
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React from "react";
 import { useQuery } from "react-query";
-import { isEmpty, isNil } from "lodash";
+import { isNil } from "lodash";
 import { api } from "../../../api/url.g";
 import { queryKeys } from "../../../lib/contants";
-import { isNotNil } from "../../../ex/utils";
-import { mf1 } from "../../../ex/numberEx";
+import { ignorePromise } from "../../../ex/utils";
+import { Replace } from "../../../layout/App";
+import { Urls } from "../../../url/url.g";
+import CartListView from "../../../view/myPage/CartListView";
 
 const CartPage = () => {
+  // TODO :: 전체 선택, 전체 삭제, 체크 박스 넣기
   const router = useRouter();
 
-  const [totalPrice, setTotalPrice] = useState(0);
-
-  const { data: cartList } = useQuery([queryKeys.cart], () => api.cartList({}), {
+  const { data: cartList, isLoading } = useQuery([queryKeys.cart], () => api.cartList({}), {
     onSuccess: (res) => {
       if (isNil(res)) {
-        return;
+        ignorePromise(() => router.replace(Urls.index.url()));
       }
-
-      let total = 0;
-      for (let i = 0; i < res.list.length; i++) {
-        total += (res.list[i]?.count ?? 0) * (res.list[i]?.price ?? 0);
-      }
-
-      setTotalPrice(total);
     },
   });
 
-  return (
-    <section className="section mt-4" id="products">
-      <div className="container sign-container">
-        <div className="row">
-          <div className="cart-item-wrapper">
-            {(isNil(cartList) || isEmpty(cartList.list)) && <li>cart is empty</li>}
-            {isNotNil(cartList) &&
-              !isEmpty(cartList.list) &&
-              cartList.list.map((cart) => {
-                return (
-                  <li className="rounded">
-                    <figure className="figure">
-                      <img className="figure-img rounded" src={cart.image.url} alt="cart-img" />
-                    </figure>
-                    <p>{cart.name}</p>
-                    <div>
-                      <div>
-                        <button type="button" className="count-button">
-                          +
-                        </button>
-                        {cart.count}
-                        <button type="button" className="count-button">
-                          -
-                        </button>
-                      </div>
-                    </div>
-                    <p className="price">${mf1(cart.count * cart.price)}</p>
-                  </li>
-                );
-              })}
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+  if (isLoading) {
+    // TODO :: 스켈레톤
+    return <></>;
+  }
+
+  if (isNil(cartList)) {
+    return <Replace url={Urls.index.url()} />;
+  }
+
+  return <CartListView list={cartList.list} />;
 };
 
 export default CartPage;
