e55007c2bac05f810b4e27d31ddcad80f098e757
dlsdk0601 <dlsdk0601@gmail.com>
2023-08-04 00:50:44 +0900

    feat: 장바구니 추가 DTO 생성

--- server/src/front/cart/dto/add-cart.dto.ts
+++ server/src/front/cart/dto/add-cart.dto.ts
@@ -1,1 +1,16 @@
-export class AddCartDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsNumber } from "class-validator";
+
+export class AddCartReqDto {
+  @ApiProperty({ description: "상품 pk", nullable: false, type: "number" })
+  @IsNumber()
+  @IsNotEmpty()
+  pk: number;
+}
+
+export class AddCartResDto {
+  @ApiProperty({ description: "장바구니 상품 pk", nullable: false, type: "number" })
+  @IsNumber()
+  @IsNotEmpty()
+  pk: number;
+}
