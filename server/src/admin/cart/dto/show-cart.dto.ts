acd795a12c8a9a5a5cf85addd623d4ee9ebeba80
dlsdk0601 <dlsdk0601@gmail.com>
2023-08-06 19:53:58 +0900

    feat: 장바구니 페이지네이션 DTO 생성

---
+++ server/src/admin/cart/dto/show-cart.dto.ts
@@ -0,0 +1,53 @@
+import { ApiExtraModels, ApiProperty, getSchemaPath } from "@nestjs/swagger";
+import { IsArray, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { PaginationDto } from "../../../type/pagination.dto";
+
+export class CartListReqDto {
+  @ApiProperty({ description: "페이지", nullable: false, type: "number" })
+  @IsNumber()
+  @IsNotEmpty()
+  page: number;
+
+  @ApiProperty({ description: "검색어", nullable: false, type: "string" })
+  @IsString()
+  @IsNotEmpty()
+  search: string;
+}
+
+export class CartListItemDto {
+  @ApiProperty({ description: "장바구니 상품 pk", nullable: false, type: "number" })
+  @IsNumber()
+  @IsNotEmpty()
+  pk: number;
+
+  @ApiProperty({ description: "유저 네임", nullable: false, type: "string" })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({ description: "유저 핸드폰 번호", nullable: false, type: "string" })
+  @IsString()
+  @IsNotEmpty()
+  phone: string | null;
+
+  @ApiProperty({ description: "상품 총 갯수", nullable: false, type: "number" })
+  @IsNumber()
+  @IsNotEmpty()
+  count: number;
+}
+
+@ApiExtraModels(CartListItemDto)
+export class CartListResDto extends PaginationDto {
+  @ApiProperty({
+    description: "아이템",
+    type: "array",
+    items: { $ref: getSchemaPath(CartListItemDto) },
+  })
+  @IsArray()
+  rows: CartListItemDto[];
+
+  constructor(data: CartListItemDto[], count: number, page: number) {
+    super(count, page);
+    this.rows = data;
+  }
+}
