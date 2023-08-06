a4a2efb6c85056d98491737bd5a1826397aeb52e
dlsdk0601 <dlsdk0601@gmail.com>
2023-08-06 19:54:15 +0900

    feat: 장바구니 페이지네이션 API 생성

--- server/src/admin/cart/cart.controller.ts
+++ server/src/admin/cart/cart.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { ApiCreatedResponse, ApiTags } from "@nestjs/swagger";
 import { CartService } from "./cart.service";
+import { CartListReqDto, CartListResDto } from "./dto/show-cart.dto";
 
-@Controller("admin")
+@Controller("/admin/cart")
+@ApiTags("어드민 - 장바구니")
 export class CartController {
   constructor(private readonly cartService: CartService) {}
+
+  @Post("cart-list")
+  @ApiCreatedResponse({ type: CartListResDto })
+  async list(@Body() body: CartListReqDto) {
+    return this.cartService.list(body);
+  }
 }
--- server/src/admin/cart/cart.service.ts
+++ server/src/admin/cart/cart.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from "@nestjs/common";
+import { Like } from "typeorm";
+import { CartListReqDto, CartListResDto } from "./dto/show-cart.dto";
+import { Cart } from "../../entities/cart.entity";
+import { LIMIT } from "../../type/pagination.dto";
 
 @Injectable()
-export class CartService {}
+export class CartService {
+  async list(body: CartListReqDto) {
+    const [carts, count] = await Cart.findAndCount({
+      take: LIMIT,
+      skip: LIMIT * (body.page - 1),
+      select: {
+        pk: true,
+      },
+      relations: {
+        user: true,
+        cart_products: true,
+      },
+      where: {
+        user: {
+          name: Like(`%${body.search}%`),
+        },
+      },
+    });
+
+    const cartList = carts.map((cart) => ({
+      pk: cart.pk,
+      name: cart.user.name,
+      phone: cart.user.phone,
+      count: cart.cart_products.length,
+    }));
+
+    return new CartListResDto(cartList, count, body.page);
+  }
+}
