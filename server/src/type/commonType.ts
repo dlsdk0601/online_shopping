830a8c085cf0023959ddddefaccc5bf3e015972b
dlsdk0601 <dlsdk0601@gmail.com>
2023-08-10 23:55:39 +0900

    feat: 상품 구매 상태값 enum 생성

--- server/src/type/commonType.ts
+++ server/src/type/commonType.ts
@@ -27,3 +27,14 @@ export enum ProductCategory {
   KIDS = "KIDS",
   ACCESSORY = "ACCESSORY",
 }
+
+export enum PurchaseLogStatus {
+  WAITING = "WAITING", // 결제 대기
+  IN_PROGRESS = "IN_PROGRESS", // 결제 진행 중
+  SUCCESS = "SUCCESS", // 결제 완료
+  CANCEL = "CANCEL", // 결제 취소
+  REFUND_WAITING = "REFUND_WAITING", // 환불 대기
+  REFUND_SUCCESS = "REFUND_SUCCESS", // 환불 완료
+  REFUND_FAIL = "REFUND_FAIL", // 환불 실패
+  FAIL = "FAIL", // 결제 실패
+}
