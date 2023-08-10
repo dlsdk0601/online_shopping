b3a9f1d9d96194b4080013a98409cc58c0357d86
dlsdk0601 <dlsdk0601@gmail.com>
2023-08-11 00:33:57 +0900

    refactor: eslint 수정

--- server/src/front/front.module.ts
+++ server/src/front/front.module.ts
@@ -8,7 +8,7 @@ import { AuthModule } from "./auth/auth.module";
 import { typeOrmModuleOptions } from "../database/ormconfig";
 import { envValidation } from "../lib/envValidation";
 import { AssetModule } from "../asset/asset.module";
-import { CartModule } from './cart/cart.module';
+import { CartModule } from "./cart/cart.module";
 
 // 전역 변수 설정 및 orm 설정을 front / admin 따로 해준다.
 // 다른 설정이 들어 갈 수도 있기 때문에
