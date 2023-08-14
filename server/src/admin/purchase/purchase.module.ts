c5487867f7c470ebcf630c4647d0b8b9a6f849b2
inaJung <ina.jung@coint.co.kr>
2023-08-14 16:55:11 +0900

    refactor: AssetService 추가

--- server/src/admin/purchase/purchase.module.ts
+++ server/src/admin/purchase/purchase.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { PurchaseService } from "./purchase.service";
 import { PurchaseController } from "./purchase.controller";
 import { Purchase, PurchaseItem } from "../../entities/Purchase.entity";
+import { AssetService } from "../../asset/asset.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Purchase, PurchaseItem])],
   controllers: [PurchaseController],
-  providers: [PurchaseService],
+  providers: [PurchaseService, AssetService],
 })
 export class PurchaseModule {}
