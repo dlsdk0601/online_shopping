28f22d84fd0630e89e985a5ef544761f4fd7a841
dlsdk0601 <dlsdk0601@gmail.com>
2023-08-15 17:52:45 +0900

    feat: purchseList API 컨트롤러 생성

--- server/src/admin/purchase/purchase.controller.ts
+++ server/src/admin/purchase/purchase.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { ApiCreatedResponse } from "@nestjs/swagger";
 import { PurchaseService } from "./purchase.service";
+import { PurchaseListReqDto, PurchaseListResDto } from "./dto/show-purchase.dto";
 
-@Controller("purchase")
+@Controller("/admin/purchase")
 export class PurchaseController {
   constructor(private readonly purchaseService: PurchaseService) {}
+
+  @Post("list-purchase")
+  @ApiCreatedResponse({ type: PurchaseListResDto })
+  async list(@Body() body: PurchaseListReqDto) {
+    return this.purchaseService.list(body);
+  }
 }
