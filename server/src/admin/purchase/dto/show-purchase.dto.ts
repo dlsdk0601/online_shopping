54dc01946ba0b06cf9d24b86d9149e5086b721c6
dlsdk0601 <dlsdk0601@gmail.com>
2023-08-15 20:18:42 +0900

    refactor: list Req 에서 검색 타입 필드 추가

--- server/src/admin/purchase/dto/show-purchase.dto.ts
+++ server/src/admin/purchase/dto/show-purchase.dto.ts
@@ -1,12 +1,26 @@
 import { ApiExtraModels, ApiProperty, getSchemaPath } from "@nestjs/swagger";
-import { IsArray, IsDate, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+import {
+  IsArray,
+  IsDate,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from "class-validator";
 import { PaginationDto } from "../../../type/pagination.dto";
+import { PurchaseSearchType } from "../../../type/commonType";
 
 export class PurchaseListReqDto {
   @ApiProperty({ description: "page number", nullable: false, type: "number" })
   @IsNumber()
   @IsNotEmpty()
   page: number;
+
+  @ApiProperty({ description: "검색 타입", nullable: true, type: "enum", enum: PurchaseSearchType })
+  @IsNotEmpty()
+  @IsEnum(PurchaseSearchType)
+  searchType: PurchaseSearchType;
 
   @ApiProperty({ description: "search", nullable: true, type: "string" })
   @IsOptional()
@@ -34,6 +48,11 @@ export class PurchaseListResPurchaseDto {
   @IsNumber()
   @IsNotEmpty()
   count: number;
+
+  @ApiProperty({ description: "주문 번호", nullable: false, type: "string" })
+  @IsString()
+  @IsNotEmpty()
+  orderCode: string;
 
   @ApiProperty({ description: "생성 일자", nullable: false, format: "date-time", type: "string" })
   @IsDate()
