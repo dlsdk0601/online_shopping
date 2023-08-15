19c71086004d056eb7207c733d1e9655ea777a49
dlsdk0601 <dlsdk0601@gmail.com>
2023-08-16 01:18:51 +0900

    refactor: 컨벤션에 맞게 endpoint 수정

--- server/src/admin/purchase/purchase.controller.ts
+++ server/src/admin/purchase/purchase.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Post } from "@nestjs/common";
-import { ApiCreatedResponse } from "@nestjs/swagger";
+import { ApiCreatedResponse, ApiTags } from "@nestjs/swagger";
 import { PurchaseService } from "./purchase.service";
 import { PurchaseListReqDto, PurchaseListResDto } from "./dto/show-purchase.dto";
 
 @Controller("/admin/purchase")
+@ApiTags("어드민 - 구매")
 export class PurchaseController {
   constructor(private readonly purchaseService: PurchaseService) {}
 
-  @Post("list-purchase")
+  @Post("purchase-list")
   @ApiCreatedResponse({ type: PurchaseListResDto })
   async list(@Body() body: PurchaseListReqDto) {
     return this.purchaseService.list(body);
