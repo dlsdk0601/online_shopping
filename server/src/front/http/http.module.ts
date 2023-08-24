049db1ffa616f7b4aabad21c0a75b1fa59075671
dlsdk0601 <dlsdk0601@gmail.com>
2023-08-24 23:18:35 +0900

    feat: 로그 기록 로직 추가

--- server/src/front/http/http.module.ts
+++ server/src/front/http/http.module.ts
@@ -1,7 +1,10 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { HttpService } from "./http.service";
+import { HttpServiceLog } from "../../entities/http-log.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([HttpServiceLog])],
   providers: [HttpService],
 })
 export class HttpModule {}
--- server/src/front/http/http.service.ts
+++ server/src/front/http/http.service.ts
@@ -1,26 +1,59 @@
 import { Injectable } from "@nestjs/common";
 import axios, { AxiosRequestConfig } from "axios";
+import { HttpServiceLog } from "../../entities/http-log.entity";
+import { HttpMethod } from "../../type/type";
 
 @Injectable()
 export class HttpService {
-  // TODO :: 공통 log 기록 여기서 하기
   async get<T>(url: string, config: AxiosRequestConfig): Promise<T | null> {
+    const startAt = Date.now();
+    const log = new HttpServiceLog();
+
+    const baseUrl = url.split("?")[0];
+    const query = url.split("?")[1];
+
+    log.request_url = baseUrl;
+    log.request_method = HttpMethod.GET;
+    log.request_query = query;
+    log.request_headers = config?.headers ?? {};
     try {
       const res = await axios.get<T>(url, config);
 
+      log.response_status_code = res.status;
+      log.response_headers = res.headers;
+      log.response_body = JSON.stringify(res.data);
       return res.data;
     } catch (e) {
+      log.exception = e instanceof Error ? e.message : "알수 없는 에러 발생";
       return null;
+    } finally {
+      log.response_duration_ms = Date.now() - startAt;
+      await log.save();
     }
   }
 
   async post<T>(url: string, config: AxiosRequestConfig, data: any): Promise<T | null> {
+    const startAt = Date.now();
+    const log = new HttpServiceLog();
+
+    log.request_url = url;
+    log.request_method = HttpMethod.GET;
+    log.request_body = JSON.stringify(data);
+    log.request_headers = config?.headers ?? {};
+
     try {
       const res = await axios.post<T>(url, data, config);
 
+      log.response_status_code = res.status;
+      log.response_headers = res.headers;
+      log.response_body = JSON.stringify(res.data);
       return res.data;
     } catch (e) {
+      log.exception = e instanceof Error ? e.message : "알수 없는 에러 발생";
       return null;
+    } finally {
+      log.response_duration_ms = Date.now() - startAt;
+      await log.save();
     }
   }
 }
