f224e38ad86bd0264f37f57c573e3c52ac529f23
dlsdk0601 <dlsdk0601@gmail.com>
2023-08-24 21:20:09 +0900

    refactor: 카카오, 네이버 로그인 S2S 에서 response interface 정의

--- server/src/front/auth/dto/kakao-auth.dto.ts
+++ server/src/front/auth/dto/kakao-auth.dto.ts
@@ -24,3 +24,59 @@ export class KakaoCodeVerifyResDto {
   @IsOptional()
   email: string | null;
 }
+
+export interface KakaoCodeVerifyRes {
+  token_type: string;
+  access_token: string;
+  id_token?: string;
+  expires_in: number;
+  refresh_token: string;
+  refresh_token_expires_in: number;
+  scope?: string;
+}
+
+export interface KakaoAccount {
+  profile_needs_agreement?: boolean;
+  profile_nickname_needs_agreement?: boolean;
+  profile_image_needs_agreement?: boolean;
+  profile?: {
+    nickname: string;
+    thumbnail_image_url: string;
+    profile_image_url: string;
+    is_default_image: string;
+  };
+  name_needs_agreement?: boolean;
+  name?: string;
+  email_needs_agreement?: boolean;
+  is_email_valid?: boolean;
+  is_email_verified?: boolean;
+  email?: string;
+  age_range_needs_agreement?: boolean;
+  age_range?: string;
+  birthyear_needs_agreement?: boolean;
+  birthyear?: string;
+  birthday_needs_agreement?: boolean;
+  birthday?: string;
+  birthday_type?: string;
+  gender_needs_agreement?: boolean;
+  gender?: string;
+  phone_number_needs_agreement?: boolean;
+  phone_number?: string;
+  ci_needs_agreement?: boolean;
+  ci?: string;
+  ci_authenticated_at?: Date;
+}
+
+export interface Partner {
+  uuid: string; // 고유 ID
+}
+
+export interface KakaoGetUserRes {
+  id: number;
+  has_signed_up?: boolean;
+  connected_at?: Date;
+  synched_at?: Date;
+  properties?: JSON;
+  kakao_account?: KakaoAccount;
+  for_partner?: Partner;
+}
--- server/src/front/auth/dto/naver-auth.dto.ts
+++ server/src/front/auth/dto/naver-auth.dto.ts
@@ -19,3 +19,29 @@ export class NaverCodeVerifyResDto {
   @IsString()
   token: string | null;
 }
+
+export interface NaverCodeVerifyRes {
+  access_token: string;
+  refresh_token: string;
+  token_type: string;
+  expires_in: string;
+}
+
+export interface NaverGetUserDataResponse {
+  id: string;
+  nickname: string;
+  name: string;
+  email: string;
+  gender: string; // - F: 여성,  M: 남성, U: 확인불가
+  age: string;
+  birthday: string; // 사용자 생일(MM-DD 형식)
+  profile_image: string; // 사용자 프로필 사진 URL
+  birthyear: string;
+  mobile: string;
+}
+
+export interface NaverGetUserData {
+  resultcode: string;
+  message: string;
+  response: NaverGetUserDataResponse;
+}
