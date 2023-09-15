a17f62a5b2137517ecfdbcc1cb7fc8869a58f2c1
dlsdk0601 <dlsdk0601@gmail.com>
2023-09-16 00:54:42 +0900

    feat: 결제 테이블 entity 생성

---
+++ server/src/entities/payment.entity.ts
@@ -0,0 +1,37 @@
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import TimeSet from "./timeSet.entity";
+import { Purchase } from "./Purchase.entity";
+import { PaymentStatus, PaymentType } from "../type/commonType";
+
+@Entity("payment")
+export class Payment extends TimeSet {
+  @PrimaryGeneratedColumn({ comment: "pk" })
+  pk: number;
+
+  @OneToOne(() => Purchase, (purchase) => purchase, {
+    nullable: false,
+    eager: true,
+  })
+  @JoinColumn({ name: "purchase_pk", referencedColumnName: "pk" })
+  purchase: Purchase;
+
+  @Column({ enum: PaymentType, type: "enum", nullable: false })
+  type: PaymentType;
+}
+
+@Entity("toss_payment")
+export class TossPayment extends TimeSet {
+  @PrimaryGeneratedColumn({ comment: "pk" })
+  pk: number;
+
+  @Column({
+    type: "varchar",
+    nullable: false,
+    length: 128,
+    comment: "거래를 구분할 수 있는 토스 고유 값",
+  })
+  payToken: string;
+
+  @Column({ enum: PaymentStatus, type: "enum", nullable: false })
+  status: PaymentStatus;
+}
