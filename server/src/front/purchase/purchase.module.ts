ca1ec333369a370c38e88a31a1834f0fa3ed8d6a
dlsdk0601 <dlsdk0601@gmail.com>
2023-09-19 01:34:39 +0900

    refactor: httpService 단 import

--- server/src/front/purchase/purchase.module.ts
+++ server/src/front/purchase/purchase.module.ts
@@ -5,10 +5,11 @@ import { PurchaseService } from "./purchase.service";
 import { PurchaseController } from "./purchase.controller";
 import { Purchase, PurchaseItem } from "../../entities/Purchase.entity";
 import { AssetService } from "../../asset/asset.service";
+import { HttpService } from "../http/http.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Purchase, PurchaseItem])],
   controllers: [PurchaseController],
-  providers: [PurchaseService, AssetService, ConfigService],
+  providers: [PurchaseService, AssetService, ConfigService, HttpService],
 })
 export class PurchaseModule {}
