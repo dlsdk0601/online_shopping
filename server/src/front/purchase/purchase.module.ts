ea1a3dfc9f5df0ce8083e1795aabd656a0d3d6e4
dlsdk0601 <dlsdk0601@gmail.com>
2023-09-22 23:57:23 +0900

    refactor: payment, tossPayment entity 추가

--- server/src/front/purchase/purchase.module.ts
+++ server/src/front/purchase/purchase.module.ts
@@ -6,9 +6,10 @@ import { PurchaseController } from "./purchase.controller";
 import { Purchase, PurchaseItem } from "../../entities/Purchase.entity";
 import { AssetService } from "../../asset/asset.service";
 import { HttpService } from "../http/http.service";
+import { Payment, TossPayment } from "../../entities/payment.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Purchase, PurchaseItem])],
+  imports: [TypeOrmModule.forFeature([Purchase, PurchaseItem, Payment, TossPayment])],
   controllers: [PurchaseController],
   providers: [PurchaseService, AssetService, ConfigService, HttpService],
 })
