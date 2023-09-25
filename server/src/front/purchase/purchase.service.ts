666bbaabebde96b0defb42a4dae956f4e6e6742c
dlsdk0601 <dlsdk0601@gmail.com>
2023-09-26 00:49:14 +0900

    refactor: 결제 승인 service 유효성 추가

--- server/src/front/purchase/purchase.service.ts
+++ server/src/front/purchase/purchase.service.ts
@@ -11,17 +11,13 @@ import { AssetService } from "../../asset/asset.service";
 import { Purchase, PurchaseItem } from "../../entities/Purchase.entity";
 import errorMessage from "../../config/errorMessage";
 import { AddPurchaseReqDto } from "./dto/add-purchase.dto";
-import { isBlank, isNotNil, makeOrderCode } from "../../ex/ex";
+import { isNotNil, makeOrderCode } from "../../ex/ex";
 import { User } from "../../entities/user.entity";
-import { PaymentType, PurchaseItemStatus } from "../../type/commonType";
+import { PurchaseItemStatus } from "../../type/commonType";
 import { CartProduct } from "../../entities/cart.entity";
-import {
-  AddTossPayPurchaseReqDto,
-  makeTossPayPurchaseReqDto,
-  makeTossPayPurchaseResDto,
-} from "./dto/toss-payment.dto";
+import { TossPaymentApproveReqDto } from "./dto/toss-payment.dto";
 import { HttpService } from "../http/http.service";
-import { Payment, TossPayment } from "../../entities/payment.entity";
+import { Payment } from "../../entities/payment.entity";
 
 @Injectable()
 export class PurchaseService {
@@ -49,6 +45,7 @@ export class PurchaseService {
     return {
       title: purchase.title,
       totalPrice: purchase.totalPrice,
+      orderId: purchase.order_code,
     };
   }
 
@@ -88,68 +85,52 @@ export class PurchaseService {
     }
   }
 
-  // 토스 페이 결제 생성
-  async addTossPayPurchase(body: AddTossPayPurchaseReqDto, user: User) {
-    const purchase = user.purchases.find((item) => item.pk === body.pk);
+  // 토스 페이 결제 승인
+  async tossPaymentApprove(body: TossPaymentApproveReqDto, user: User) {
+    const purchase = user.purchases.find((item) => item.order_code === body.orderCode);
 
     if (isNil(purchase)) {
       throw new BadRequestException(errorMessage.BAD_REQUEST);
     }
 
-    const secretKey = this.configService.get<string>("TOSS_PAYMENT_SECRET_KEY") ?? "";
-    const base64SecretKey = Buffer.from(`${secretKey}:`).toString("base64");
-
-    // 이미 post 안에 try ~ catch 가 있기 때문에 여기서 굳이 또 나누지 않는다.
-    const res = await this.httpService.post<makeTossPayPurchaseResDto, makeTossPayPurchaseReqDto>(
-      this.addTossPayPurchaseEndPoint,
-      {
-        headers: {
-          "Authorization": `Basic ${base64SecretKey}`,
-          "Content-Type": "application/json",
-        },
-      },
-      {
-        orderNo: purchase.order_code,
-        amount: purchase.totalPrice,
-        amountTaxFree: 0,
-        productDesc: body.productDesc,
-        apiKey: this.configService.get<string>("TOSS_PAYMENT_CLIENT_API_KEY") ?? "",
-        autoExecute: true,
-        callbackVersion: this.TOSS_CALLBACK_VERSION,
-        resultCallback: this.configService.get<string>("TOSS_PAYMENT_CALLBACK_URL") ?? "",
-        retUrl: this.configService.get<string>("TOSS_PAYMENT_RESULT_URL") ?? "",
-        retCancelUrl: this.configService.get<string>("TOSS_PAYMENT_FAIL_URL") ?? "",
-      }
-    );
-
-    if (isNil(res)) {
-      return new InternalServerErrorException(errorMessage.INTERNAL_FAILED);
+    const isAllWaiting = purchase.purchase_items
+      .map((item) => item.status)
+      .every((item) => item === PurchaseItemStatus.WAITING);
+    if (!isAllWaiting) {
+      throw new BadRequestException(errorMessage.ALREADY_PAID);
     }
+
+    if (body.amount !== purchase.totalPrice) {
+      throw new BadRequestException(errorMessage.NOT_EQUAL_PRICE);
+    }
+
+    const base64SecretKey = Buffer.from(`${this.tossSecretKey}:`).toString("base64");
 
     try {
       const payment = new Payment();
       payment.purchase = purchase;
-      payment.type = PaymentType.TOSS;
       await payment.save();
 
-      const tossPayment = new TossPayment();
+      // const tossPayment = new TossPayment();
+      //
+      // tossPayment.code = res.code;
+      // tossPayment.checkout_page = res?.checkoutPage ?? "";
+      // tossPayment.pay_token = res?.payToken ?? "";
+      // tossPayment.msg = res.msg;
+      // tossPayment.error_code = res.errorCode;
+      // tossPayment.payment = payment;
 
-      tossPayment.code = res.code;
-      tossPayment.checkout_page = res?.checkoutPage ?? "";
-      tossPayment.pay_token = res?.payToken ?? "";
-      tossPayment.msg = res.msg;
-      tossPayment.error_code = res.errorCode;
-      tossPayment.payment = payment;
-
-      await tossPayment.save();
+      // await tossPayment.save();
     } catch (e) {
       throw new InternalServerErrorException(errorMessage.INTERNAL_FAILED);
     }
 
-    if (res.code !== 0 || isBlank(res.checkoutPage)) {
-      throw new InternalServerErrorException(errorMessage.INTERNAL_FAILED);
-    }
+    // if (res.code !== 0 || isBlank(res.checkoutPage)) {
+    //   throw new InternalServerErrorException(errorMessage.INTERNAL_FAILED);
+    // }
 
-    return { checkoutPage: res.checkoutPage };
+    return { checkoutPage: "" };
   }
+
+  async tossPayCallback(body) {}
 }
