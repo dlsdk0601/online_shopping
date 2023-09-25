6654548125a6940bc275534c3174f48d2fe19255
dlsdk0601 <dlsdk0601@gmail.com>
2023-09-25 22:28:50 +0900

    feat: toss payment 키 두개 추가

--- front/src/lib/config.ts
+++ front/src/lib/config.ts
@@ -10,6 +10,10 @@ class Config {
   naver_redirect_url: string = process.env.NEXT_PUBLIC_NAVER_REDIRECT_URL ?? "";
   naver_client_id: string = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID ?? "";
   naver_state: string = process.env.NEXT_PUBLIC_NAVER_STATE ?? "";
+  toss_client_key: string =
+    process.env.NEXT_PUBLIC_TOSS_PAYMENT_CLIENT_KEY ?? "test_ck_DpexMgkW36m7oJewMgw8GbR5ozO0";
+  toss_secret_key: string =
+    process.env.NEXT_PUBLIC_TOSS_PAYMENT_SECRET_KEY ?? "test_sk_LkKEypNArWQ1Lv9aaPe3lmeaxYG5";
 }
 
 export const baseConfig = new Config();
