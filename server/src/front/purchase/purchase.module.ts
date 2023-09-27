4404e50714aa518724c4b9b837ee00f2ab84f5a3
dlsdk0601 <dlsdk0601@gmail.com>
2023-09-28 02:59:04 +0900

    refactor: TossPaymentApprove, TossPaymentApproveCard entity import 추가

--- server/src/front/purchase/purchase.module.ts
+++ server/src/front/purchase/purchase.module.ts
@@ -6,10 +6,18 @@ import { PurchaseController } from "./purchase.controller";
 import { Purchase, PurchaseItem } from "../../entities/Purchase.entity";
 import { AssetService } from "../../asset/asset.service";
 import { HttpService } from "../http/http.service";
-import { Payment, TossPayment } from "../../entities/payment.entity";
+import { Payment, TossPaymentApprove, TossPaymentApproveCard } from "../../entities/payment.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Purchase, PurchaseItem, Payment, TossPayment])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Purchase,
+      PurchaseItem,
+      Payment,
+      TossPaymentApprove,
+      TossPaymentApproveCard,
+    ]),
+  ],
   controllers: [PurchaseController],
   providers: [PurchaseService, AssetService, ConfigService, HttpService],
 })
