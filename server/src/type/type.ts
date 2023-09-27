ce9841c428cfcf7b3e08a982a92024c1a2b6fe72
dlsdk0601 <dlsdk0601@gmail.com>
2023-09-28 02:56:22 +0900

    feat: 카드 결제의 매입 상태 enum 생성

--- server/src/type/type.ts
+++ server/src/type/type.ts
@@ -37,3 +37,11 @@ export enum HttpMethod {
   PUT = "PUT",
   PATCH = "PATCH",
 }
+
+export enum TossPaymentCardAcquireStatus {
+  READY = "READY", // 아직 매입 요청이 안 된 상태입니다.
+  REQUESTED = "REQUESTED", // 매입이 요청된 상태입니다.
+  COMPLETED = "COMPLETED", // 요청된 매입이 완료된 상태입니다.
+  CANCEL_REQUESTED = "CANCEL_REQUESTED", // 매입 취소가 요청된 상태입니다.
+  CANCELED = "CANCELED", // 요청된 매입 취소가 완료된 상태입니다.
+}
