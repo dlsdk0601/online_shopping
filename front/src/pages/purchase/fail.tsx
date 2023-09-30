283e37e95fe1dc15b9589b458f40474031071358
dlsdk0601 <dlsdk0601@gmail.com>
2023-10-01 01:49:49 +0900

    isApi 추가

--- front/src/pages/purchase/fail.tsx
+++ front/src/pages/purchase/fail.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
 import { isBlank, queryFilter } from "../../ex/utils";
 
 const Fail = () => {
   const router = useRouter();
+  const isApi = useRef(false); // API 통신 후 막기
   const [message, setMessage] = useState("");
 
   useEffect(() => {
@@ -11,13 +12,19 @@ const Fail = () => {
       return;
     }
 
+    if (isApi) {
+      return;
+    }
+
     const code = queryFilter(router.query.code);
     const queryMessage = queryFilter(router.query.message);
     const orderId = queryFilter(router.query.orderId);
 
+    // API 통신 에서 메세지 받을지 여기서 처리할지 결정
     if (isBlank(message)) {
       // TODO :: 문구 수정
       setMessage("알 수 없는 에러 발생.");
+      return;
     }
 
     setMessage(queryMessage);
