d47c70e4f0a4c0bbe72871255e578d23d1827174
dlsdk0601 <dlsdk0601@gmail.com>
2023-10-01 01:44:24 +0900

    refactor: 결제 실패 페이지추가

--- front/src/pages/purchase/fail.tsx
+++ front/src/pages/purchase/fail.tsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { isBlank, queryFilter } from "../../ex/utils";
+
 const Fail = () => {
-  return <div>fail</div>;
+  const router = useRouter();
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    if (!router.isReady) {
+      return;
+    }
+
+    const code = queryFilter(router.query.code);
+    const queryMessage = queryFilter(router.query.message);
+    const orderId = queryFilter(router.query.orderId);
+
+    if (isBlank(message)) {
+      // TODO :: 문구 수정
+      setMessage("알 수 없는 에러 발생.");
+    }
+
+    setMessage(queryMessage);
+  }, [router.isReady]);
+
+  return <div>{message}</div>;
 };
 
 export default Fail;
