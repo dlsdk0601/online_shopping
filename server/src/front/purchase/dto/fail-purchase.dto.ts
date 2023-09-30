7900cfbd661b54ef1851756107912b7c519d527b
dlsdk0601 <dlsdk0601@gmail.com>
2023-10-01 01:41:09 +0900

    feat: 결제 생성 실패 API DTO 생성

---
+++ server/src/front/purchase/dto/fail-purchase.dto.ts
@@ -0,0 +1,26 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class FailPurchaseReqDto {
+  @ApiProperty({ type: "string", nullable: false, description: "에러 코드" })
+  @IsString()
+  @IsNotEmpty()
+  code: string;
+
+  @ApiProperty({ type: "string", nullable: false, description: "에러 메세지" })
+  @IsString()
+  @IsNotEmpty()
+  message: string;
+
+  @ApiProperty({ type: "string", nullable: false, description: "주문 번호" })
+  @IsString()
+  @IsNotEmpty()
+  orderCode: string;
+}
+
+export class FailPurchaseResDto {
+  @ApiProperty({ type: "number", nullable: false, description: "pk" })
+  @IsNumber()
+  @IsNotEmpty()
+  pk: number;
+}
