efda7880d0ed9c7ab639c04ae6ecef72c438e610
dlsdk0601 <dlsdk0601@gmail.com>
2023-10-02 18:58:26 +0900

    refactor: 결제 성공 페이지에서 home 버튼 추가 및 문구 추가

--- front/src/pages/purchase/success/[pk].tsx
+++ front/src/pages/purchase/success/[pk].tsx
@@ -1,5 +1,34 @@
+import React from "react";
+import Link from "next/link";
+import { Urls } from "../../../url/url.g";
+
 const Success = () => {
-  return <div>sucess</div>;
+  return (
+    <>
+      <div className="page-heading" id="top">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="inner-content">
+                <h2>Single Product Page</h2>
+                <span>Awesome &amp; Creative HTML CSS layout by TemplateMo</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section className="section" id="product">
+        <div className="container">
+          <p className="success-text">PAYMENT SUCCESS</p>
+          <div className="success-button-box">
+            <Link className="success-button" href={Urls.index.url()}>
+              HOME
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Success;
