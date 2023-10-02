0cfc940015b7ad4b7fbd5e516d946cd020e0310d
dlsdk0601 <dlsdk0601@gmail.com>
2023-10-02 17:59:49 +0900

    refactor: 주석 설명 추가

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -10,6 +10,7 @@ import { getDeviceInfo } from "../ex/ex";
 export async function middle(req: Request, res: Response, next: NextFunction) {
   if (!req.path.includes("admin")) {
     // front
+    // front 에서는 필요한 API 에서만 useGuard 를 사용
     return next();
   }
 
@@ -18,6 +19,7 @@ export async function middle(req: Request, res: Response, next: NextFunction) {
     return next();
   }
 
+  // 어드민에서 모든 API 에 useGuard 하기 귀찮으니까 admin 은 여기서 한번에 처리한다.
   const bearerToken = req.headers.authorization ?? null;
 
   if (isNil(bearerToken)) {
