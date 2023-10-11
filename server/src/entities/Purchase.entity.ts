3648f9f5dc4667349c2f99044bd09fd14c49724c
dlsdk0601 <dlsdk0601@gmail.com>
2023-10-12 01:00:50 +0900

    refactor: nullable 한 payment 에 null 추가

--- server/src/entities/Purchase.entity.ts
+++ server/src/entities/Purchase.entity.ts
@@ -43,7 +43,7 @@ export class Purchase extends BaseEntity {
   @OneToOne(() => Payment, (payment) => payment.purchase, {
     nullable: true,
   })
-  payment: Payment;
+  payment: Payment | null;
 
   get totalPrice(): number {
     let total = 0;
