589d2c77d11067ced4d48f75666a8fcde2a1b657
dlsdk0601 <dlsdk0601@gmail.com>
2023-10-23 13:24:41 +0900

    refactor: show order API 연동

--- front/src/pages/my-page/order/show/[pk].tsx
+++ front/src/pages/my-page/order/show/[pk].tsx
@@ -1,4 +1,28 @@
+import { useQuery } from "react-query";
+import { useRouter } from "next/router";
+import { isNil } from "lodash";
+import { isNotNil, validatePk } from "../../../../ex/utils";
+import { api } from "../../../../api/url.g";
+
 const OrderShowPage = () => {
+  const router = useRouter();
+  const pk = validatePk(router.query.pk);
+
+  if (isNil(pk)) {
+    return <div>pk 실패</div>;
+  }
+
+  const { data, isLoading } = useQuery(["order", pk], () => api.showOrder({ pk }), {
+    onSuccess: (res) => {},
+    staleTime: 5000,
+    enabled: isNotNil(pk),
+  });
+
+  if (isLoading || isNil(data)) {
+    // TODO :: skeleton
+    return <></>;
+  }
+
   return <div>test</div>;
 };
 
