0048c7baceba8a11ebc4b11c07b199bcb21a6a88
dlsdk0601 <dlsdk0601@gmail.com>
2023-10-29 15:28:48 +0900

    refactor: 주문 내역 페이지 퍼블 수정

--- front/src/pages/my-page/order/index.tsx
+++ front/src/pages/my-page/order/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import React from "react";
-import { isNil } from "lodash";
+import { isEmpty, isNil } from "lodash";
 import { validatePageQuery } from "../../../ex/utils";
 import { useOrders } from "../../../hooks/useOrders";
 import { Replace } from "../../../layout/App";
@@ -9,6 +9,8 @@ import { d1 } from "../../../ex/dateEx";
 import PaginationBarView from "../../../view/PaginationBarView";
 import { Urls } from "../../../url/url.g";
 import MyPageListSkeleton from "../../../view/skeleton/MyPageListSkeleton";
+import { paymentStatusEnumToLabel } from "../../../api/enum";
+import { TossPaymentStatus } from "../../../api/enum.g";
 
 const OrderPage = () => {
   const router = useRouter();
@@ -25,26 +27,32 @@ const OrderPage = () => {
   }
 
   return (
-    <section className="section" id="products">
-      <div className="container">
+    <section className="section mt-4" id="products">
+      <div className="container sign-container">
         <div className="row">
           <ul className="cart-item-wrapper">
-            {pagination.rows.map((item) => {
-              return (
-                <li
-                  className="order-item"
-                  onClick={() =>
-                    router.push(Urls["my-page"].order.show["[pk]"].url({ pk: item.pk }))
-                  }
-                >
-                  <p>{d1(item.createAt)}</p>
-                  <p>{item.orderCode}</p>
-                  <p>${mf2(item.price)}</p>
-                  <p>{item.method}</p>
-                  <p>{item.status}</p>
-                </li>
-              );
-            })}
+            {isEmpty(pagination) && (
+              <li>
+                <span>order is empty</span>
+              </li>
+            )}
+            {!isEmpty(pagination) &&
+              pagination.rows.map((item) => {
+                return (
+                  <li
+                    className="order-item rounded"
+                    onClick={() =>
+                      router.push(Urls["my-page"].order.show["[pk]"].url({ pk: item.pk }))
+                    }
+                  >
+                    <p>{d1(item.createAt)}</p>
+                    <p>{item.orderCode}</p>
+                    <p>${mf2(item.price)}</p>
+                    <p>{item.method}</p>
+                    <p>{paymentStatusEnumToLabel(item.status as TossPaymentStatus)}</p>
+                  </li>
+                );
+              })}
           </ul>
           <PaginationBarView pagination={pagination} />
         </div>
