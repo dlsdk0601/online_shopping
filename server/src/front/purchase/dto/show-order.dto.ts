37e4fc06d55a311d04477ae0e769359abf76bd29
dlsdk0601 <dlsdk0601@gmail.com>
2023-10-29 15:52:11 +0900

    refactor: DTO 에 구매 상품 리스트 DTO 추가

--- server/src/front/purchase/dto/show-order.dto.ts
+++ server/src/front/purchase/dto/show-order.dto.ts
@@ -1,5 +1,7 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { ApiExtraModels, ApiProperty, getSchemaPath } from "@nestjs/swagger";
+import { IsArray, IsEnum, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { PurchaseItemStatus } from "../../../type/commonType";
+import { FileSetDto } from "../../../asset/dto/fileSet.dto";
 
 export class ShowOrderReqDto {
   @ApiProperty({ description: "구매 pk", nullable: false, type: "number" })
@@ -8,6 +10,43 @@ export class ShowOrderReqDto {
   pk: number;
 }
 
+@ApiExtraModels(FileSetDto)
+export class ShowOrderProductItem {
+  @ApiProperty({ description: "상품 pk", nullable: false, type: "number" })
+  @IsNumber()
+  @IsNotEmpty()
+  pk: number;
+
+  @ApiProperty({ description: "상품명", nullable: false, type: "string" })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({ description: "수량", nullable: false, type: "number" })
+  @IsNumber()
+  @IsNotEmpty()
+  count: number;
+
+  @ApiProperty({ description: "가격", nullable: false, type: "number" })
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
+
+  @ApiProperty({ description: "상태", nullable: false, type: "enum", enum: PurchaseItemStatus })
+  @IsEnum(PurchaseItemStatus)
+  @IsNotEmpty()
+  status: PurchaseItemStatus;
+
+  @ApiProperty({
+    description: "이미지",
+    nullable: false,
+    items: { $ref: getSchemaPath(FileSetDto) },
+  })
+  @IsNotEmpty()
+  image: FileSetDto;
+}
+
+@ApiExtraModels(ShowOrderProductItem)
 export class ShowOrderResDto {
   @ApiProperty({ description: "구매 pk", nullable: false, type: "number" })
   @IsNumber()
@@ -28,4 +67,14 @@ export class ShowOrderResDto {
   @IsString()
   @IsNotEmpty()
   orderId: string;
+
+  @ApiProperty({
+    description: "구매 상품 리스트",
+    nullable: false,
+    type: "array",
+    items: { $ref: getSchemaPath(ShowOrderProductItem) },
+  })
+  @IsArray()
+  @IsNotEmpty()
+  products: ShowOrderProductItem[];
 }
