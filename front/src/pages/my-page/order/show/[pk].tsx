0e2579c9d302fe85f9d10b991c507a80a9181e16
dlsdk0601 <dlsdk0601@gmail.com>
2023-11-01 00:01:19 +0900

    refactor: 주문 내역 상세 퍼블 추가

--- front/src/pages/my-page/order/show/[pk].tsx
+++ front/src/pages/my-page/order/show/[pk].tsx
@@ -4,6 +4,7 @@ import { isNil } from "lodash";
 import { isNotNil, validatePk } from "../../../../ex/utils";
 import { api } from "../../../../api/url.g";
 import { queryKeys } from "../../../../lib/contants";
+import { mf2 } from "../../../../ex/numberEx";
 
 const OrderShowPage = () => {
   const router = useRouter();
@@ -28,14 +29,56 @@ const OrderShowPage = () => {
   return (
     <section className="section" id="product">
       <div className="container">
-        <div className="row">
-          <div className="col-lg-8">
-            <p>{data.pk}</p>
-            <p>{data.orderId}</p>
-            <p>{data.title}</p>
-            <p>{data.totalPrice}</p>
-          </div>
+        <div className="w-50 mx-auto">
+          <ul className="d-flex justify-content-between align-items-center flex-wrap">
+            <li className="w-50 px-3 mb-2">주문 번호:</li>
+            <li className="w-50 text-right px-3 mb-2">{data.orderId}</li>
+            <li className="w-50 px-3 mb-2">주문 명:</li>
+            <li className="w-50 text-right px-3 mb-2">{data.title}</li>
+            <li className="w-50 px-3 mb-2">결제 가격:</li>
+            <li className="w-50 text-right px-3 mb-2">${mf2(data.totalPrice)}</li>
+            <li className="w-50 px-3 mb-2">결제 상태:</li>
+            <li className="w-50 text-right px-3 mb-2">상태</li>
+          </ul>
         </div>
+        <ul className="w-75 mx-auto mt-3">
+          <li className="d-flex justify-content-between align-items-center mb-2">
+            <p style={{ width: "20%" }} className="text-center">
+              제품
+            </p>
+            <p style={{ width: "20%" }} className="text-center">
+              제품명
+            </p>
+            <p style={{ width: "20%" }} className="text-center">
+              수량
+            </p>
+            <p style={{ width: "20%" }} className="text-center">
+              가격
+            </p>
+            <p style={{ width: "20%" }} className="text-center">
+              상태
+            </p>
+          </li>
+          {data.products.map((item) => (
+            <li className="d-flex justify-content-between align-items-center mb-2">
+              <figure style={{ height: "100px", width: "20%" }} className="text-center">
+                <img style={{ height: "100%" }} src={item.image.url} alt="product" />
+              </figure>
+              <p style={{ width: "20%" }} className="text-center">
+                {item.name}
+              </p>
+              <p style={{ width: "20%" }} className="text-center">
+                {item.count}
+              </p>
+              <p style={{ width: "20%" }} className="text-center">
+                {item.price}
+              </p>
+              <p style={{ width: "20%" }} className="text-center">
+                {item.status}
+              </p>
+            </li>
+          ))}
+        </ul>
       </div>
     </section>
   );
