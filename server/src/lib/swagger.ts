6d5980d6d1d6d15096114939b9461df9bb743962
dlsdk0601 <dlsdk0601@gmail.com>
2023-11-22 23:32:57 +0900

    refactor: constant() => config

--- server/src/lib/swagger.ts
+++ server/src/lib/swagger.ts
@@ -2,9 +2,9 @@ import { INestApplication } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { NestFactory } from "@nestjs/core";
 import { writeFileSync } from "fs";
-import constant from "../config/constant";
 import { FrontAppModule } from "../front/front.module";
 import { AdminAppModule } from "../admin/admin.module";
+import { config } from "../config";
 
 export const setupSwagger = async (app: INestApplication) => {
   const frontApp = await NestFactory.create(FrontAppModule);
@@ -14,7 +14,7 @@ export const setupSwagger = async (app: INestApplication) => {
   const options = new DocumentBuilder()
     .setTitle("online-shopping")
     .setDescription("Boiler-Plate API")
-    .setVersion(constant().Version)
+    .setVersion(config.version)
     .addBearerAuth(
       {
         type: "http",
@@ -33,11 +33,13 @@ export const setupSwagger = async (app: INestApplication) => {
     encoding: "utf-8",
   });
 
+  // front-api
   const frontDocument = SwaggerModule.createDocument(frontApp, options);
   writeFileSync("./front-api.json", JSON.stringify(frontDocument), {
     encoding: "utf-8",
   });
 
+  // admin-api
   const adminDocument = SwaggerModule.createDocument(adminApp, options);
   writeFileSync("./admin-api.json", JSON.stringify(adminDocument), {
     encoding: "utf-8",
