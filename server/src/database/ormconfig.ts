1877d7ae9fbfb89880fdce537eee91b43da8dfcb
dlsdk0601 <dlsdk0601@gmail.com>
2023-11-22 23:28:22 +0900

    refactor: config 에서 환경 변수값 받아오는걸로 수정

--- server/src/database/ormconfig.ts
+++ server/src/database/ormconfig.ts
@@ -1,32 +1,27 @@
-import dotenv from "dotenv";
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { DataSource, DataSourceOptions } from "typeorm";
-import { ConfigService } from "@nestjs/config";
-
-dotenv.config();
-
-const configService = new ConfigService();
+import { config } from "../config";
 
 // 두가지 설정에서 경로를 다르게 읽어서 따로 경로를 설정해준다.
 const dataSourceOptions: DataSourceOptions = {
   type: "postgres",
-  host: configService.get<string>("POSTGRES_HOST"),
-  port: configService.get<number>("POSTGRES_PORT"),
-  username: configService.get<string>("POSTGRES_USERNAME"),
-  password: configService.get<string>("POSTGRES_PASSWORD"),
-  database: configService.get<string>("POSTGRES_DATABASE"),
-  synchronize: configService.get<boolean>("POSTGRES_SYNCHRONIZE"),
+  host: config.host,
+  port: config.dbPort,
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  synchronize: config.synchronize,
   migrationsTableName: "migrations",
   migrations: ["migrations/*.ts"],
   entities: ["src/**/*.entity.ts"],
-  logging: configService.get<boolean>("POSTGRES_LOGGING"),
+  logging: config.logging,
 };
 
 export const typeOrmModuleOptions: TypeOrmModuleOptions = {
   ...dataSourceOptions,
   migrations: [`${__dirname}/**/*.ts`],
   entities: [`${__dirname}/**/*.entity.ts`],
-  autoLoadEntities: true,
+  autoLoadEntities: config.autoLoadEntities,
 };
 
 export default new DataSource(dataSourceOptions);
