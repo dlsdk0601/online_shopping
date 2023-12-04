3c691038c2eaeb4039b0b100801830ec9df044b2
dlsdk0601 <dlsdk0601@gmail.com>
2023-12-05 02:11:01 +0900

    refactor: ScheduleModule import 및 TaskService providers 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { ScheduleModule } from "@nestjs/schedule";
 import { FrontAppModule } from "./front/front.module";
 import { AdminAppModule } from "./admin/admin.module";
+import { TasksService } from "./task.service";
 
 @Module({
-  imports: [AdminAppModule, FrontAppModule],
+  imports: [ScheduleModule.forRoot(), AdminAppModule, FrontAppModule],
+  providers: [TasksService],
 })
 export class AppModule {}
