95efe34bc04ddc8a68687e3f73dc7091c3ae989f
dlsdk0601 <dlsdk0601@gmail.com>
2023-12-11 00:46:26 +0900

    refactor: provider 에 HttpService 추가

--- server/src/admin/purchase/purchase.module.ts
+++ server/src/admin/purchase/purchase.module.ts
@@ -4,10 +4,11 @@ import { PurchaseService } from "./purchase.service";
 import { PurchaseController } from "./purchase.controller";
 import { Purchase, PurchaseItem } from "../../entities/Purchase.entity";
 import { AssetService } from "../../asset/asset.service";
+import { HttpService } from "../../front/http/http.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Purchase, PurchaseItem])],
   controllers: [PurchaseController],
-  providers: [PurchaseService, AssetService],
+  providers: [PurchaseService, AssetService, HttpService],
 })
 export class PurchaseModule {}
