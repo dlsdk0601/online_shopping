7e1e570aee667df399adb26d41d16db5c3896eb5
dlsdk0601 <dlsdk0601@gmail.com>
2023-12-11 18:02:36 +0900

    refactor: refund list, show API 연동

--- admin/src/pages/order/refund/edit/[pk].tsx
+++ admin/src/pages/order/refund/edit/[pk].tsx
@@ -1,7 +1,88 @@
-import React from "react";
+import React, { memo, useEffect, useState } from "react";
+import { isNil } from "lodash";
+import ShowContainer from "../../../../layout/ShowContainer";
+import { queryKeys } from "../../../../lib/contants";
+import { api } from "../../../../api/url.g";
+import useValueField from "../../../../hooks/useValueField";
+import { RefundItem, ShowRefundRes } from "../../../../api/type.g";
+import CardFormView from "../../../../components/tailwindEx/CardFormView";
+import { ReadOnlyTextView, TextFieldView } from "../../../../components/field/field";
+import { isNotNil } from "../../../../ex/utils";
+import { d1 } from "../../../../ex/dateEx";
+import { mf2 } from "../../../../ex/numberEx";
 
 const RefundEditPage = () => {
-  return <div>refund edit</div>;
+  return (
+    <ShowContainer queryKey={queryKeys.refund} api={api.showRefund} Component={RefundEditView} />
+  );
 };
+
+const RefundEditView = memo((props: { data?: ShowRefundRes }) => {
+  const [name, setName] = useValueField("", "유저 이름");
+  const [phone, setPhone] = useValueField("", "유저 휴대폰");
+  const [list, setList] = useState<RefundItem[]>([]);
+
+  useEffect(() => {
+    if (isNil(props.data)) {
+      return;
+    }
+
+    setName.set(props.data.name);
+    setPhone.set(props.data.phone);
+    setList([...props.data.purchaseItems]);
+  }, [props.data]);
+
+  return (
+    <div className="w-full px-4">
+      <CardFormView title="환불 정보">
+        <TextFieldView
+          value={name}
+          onChange={(value) => setName.set(value)}
+          isShowingLabel
+          disabled
+        />
+        <TextFieldView
+          value={phone}
+          onChange={(value) => setPhone.set(value)}
+          isShowingLabel
+          disabled
+        />
+        <ReadOnlyTextView value={props.data?.orderCode ?? ""} label="주문 번호" />
+        <ReadOnlyTextView
+          value={isNotNil(props.data?.createAt) ? d1(props.data?.createAt) : ""}
+          label="주문 번호"
+        />
+        <div className="mt-2 flex w-11/12 justify-between">
+          <p className="text-m font-bold uppercase text-blueGray-600">구매 상품 리스트</p>
+        </div>
+        <ul className="w-11/12">
+          <li className="mb-1 flex items-center justify-between rounded border-2 p-5 text-center">
+            <p className="w-1/12">이미지</p>
+            <p className="w-1/12">상품이름</p>
+            <p className="w-1/12">가격</p>
+            <p className="w-1/12">갯수</p>
+            <p className="w-1/12">가격</p>
+          </li>
+          {list.map((product) => {
+            return (
+              <li
+                key={product.pk}
+                className="mb-3 flex items-center justify-between rounded border p-5 text-center"
+              >
+                <div className="w-1/12">
+                  <img src={product.image.url} alt="product" className="w-full" />
+                </div>
+                <p className="w-1/12">{product.name}</p>
+                <p className="w-1/12">{mf2(product.price)}</p>
+                <p className="w-1/12">{product.count}</p>
+                <p className="w-1/12">$ {mf2(product.price * product.count)}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </CardFormView>
+    </div>
+  );
+});
 
 export default RefundEditPage;
--- admin/src/pages/order/refund/index.tsx
+++ admin/src/pages/order/refund/index.tsx
@@ -1,5 +1,98 @@
+import { useRouter } from "next/router";
+import { useCallback, useState } from "react";
+import { useMutation } from "react-query";
+import { isNil } from "lodash";
+import useValueField from "../../../hooks/useValueField";
+import { PurchaseSearchType } from "../../../api/enum.g";
+import { RefundListReq, RefundListRes, RefundListResRefund } from "../../../api/type.g";
+import { api } from "../../../api/url.g";
+import useIsReady from "../../../hooks/useIsReady";
+import { labelToPurchaseSearchType } from "../../../api/enum";
+import { codecNumber, codecString, ignorePromise } from "../../../ex/utils";
+import { Urls } from "../../../url/url.g";
+import SearchBarView from "../../../components/table/searchBarView";
+import SelectView from "../../../view/SelectView";
+import { PaginationTableView } from "../../../components/table/Table";
+import { d2 } from "../../../ex/dateEx";
+
 const RefundListPage = () => {
-  return <div>refundList</div>;
+  const router = useRouter();
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useValueField("", "검색어");
+  const [searchType, setSearchType] = useState<PurchaseSearchType | null>(null);
+  const [refundList, setRefundList] = useState<RefundListRes | null>(null);
+
+  const { mutate: onSearchApi } = useMutation((req: RefundListReq) => api.refundList(req), {
+    onSuccess: (res) => {
+      if (isNil(res)) {
+        return;
+      }
+
+      setRefundList({ ...res });
+    },
+  });
+
+  useIsReady(() => {
+    const { page, search, searchType } = router.query;
+
+    const parsedPage = codecNumber(page) ?? 1;
+    const parsedSearch = codecString(search);
+    const parsedSearchType = labelToPurchaseSearchType(codecString(searchType)) ?? null;
+
+    setPage(parsedPage);
+    setSearch.set(parsedSearch);
+    setSearchType(parsedSearchType);
+    onSearchApi({ page: parsedPage, search: parsedSearch, searchType: parsedSearchType });
+  });
+
+  const onSearch = useCallback(() => {
+    const query = {
+      page: 1,
+      search: search.value,
+      searchType,
+    };
+
+    ignorePromise(() => router.push(Urls.order.index.url(query)));
+  }, [page, search, searchType]);
+
+  return (
+    <div className="mt-4">
+      <div className="mb-12 w-full rounded px-4">
+        <SearchBarView
+          onSubmit={() => onSearch()}
+          value={search.value}
+          onChange={(e) => setSearch.set(e.target.value)}
+        >
+          <SelectView<PurchaseSearchType | null>
+            searchType={searchType}
+            onChangeType={(type) => setSearchType(type)}
+            options={[
+              [null, "전체"],
+              [PurchaseSearchType.NAME, "이름"],
+              [PurchaseSearchType.PHONE, "휴대폰"],
+              [PurchaseSearchType.ORDER_CODE, "주문번호"],
+            ]}
+          />
+        </SearchBarView>
+        <PaginationTableView<RefundListResRefund>
+          title="상품 구매 리스트"
+          pagination={refundList ?? null}
+          mapper={(value) => [
+            ["주문 번호", value.orderCode],
+            ["주문자", value.name],
+            ["주문자 연락처", value.phone],
+            ["환불 금액", value.cancelPrice],
+            ["결제 수단", value.method],
+            ["생성 일자", d2(value.createAt)],
+            ["환불 일자", d2(value.canceledAt)],
+          ]}
+          links={(refundList?.rows ?? []).map((cancel) =>
+            Urls.order.refund.edit["[pk]"].url({ pk: cancel.pk }),
+          )}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default RefundListPage;
