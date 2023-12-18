c2f4334f272e91d12967b64633d73f2e1981f60a
dlsdk0601 <dlsdk0601@gmail.com>
2023-12-19 01:13:15 +0900

    refactor: prettier

--- server/src/admin/admin.module.ts
+++ server/src/admin/admin.module.ts
@@ -11,8 +11,8 @@ import { CartModule } from "./cart/cart.module";
 import { envValidation } from "../lib/envValidation";
 import { typeOrmModuleOptions } from "../database/ormconfig";
 import { AssetModule } from "../asset/asset.module";
-import { PurchaseModule } from './purchase/purchase.module';
-import { HomeModule } from './home/home.module';
+import { PurchaseModule } from "./purchase/purchase.module";
+import { HomeModule } from "./home/home.module";
 
 // 전역 변수 설정 및 orm 설정을 front / admin 따로 해준다.
 // 다른 설정이 들어 갈 수도 있기 때문에
