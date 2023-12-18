f125395242c63a8d2de7ed07749a9e67e3450151
dlsdk0601 <dlsdk0601@gmail.com>
2023-12-18 22:44:30 +0900

    refactor: errorCode 렌더링 추가

--- front/src/pages/purchase/fail.tsx
+++ front/src/pages/purchase/fail.tsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useMutation } from "react-query";
 import { isNil } from "lodash";
-import { isBlank, queryFilter } from "../../ex/utils";
+import { codecString } from "../../ex/utils";
 import { api } from "../../api/url.g";
 import { FailPurchaseReq } from "../../api/type.g";
 
 const FailPage = () => {
   const router = useRouter();
   const [message, setMessage] = useState("");
+  const [errorCode, setErrorCode] = useState("");
 
   const { mutate: onApiPurchaseFail } = useMutation(
     (req: FailPurchaseReq) => api.failPurchase(req),
@@ -20,6 +21,7 @@ const FailPage = () => {
 
         // front 에서 바로 처리 해도 되지만, 일반 API 통신과 같은 로직을 사용하기 위해 API 로 받아서 처리
         setMessage(res.message);
+        setErrorCode(res.code);
       },
     },
   );
@@ -29,19 +31,18 @@ const FailPage = () => {
       return;
     }
 
-    const code = queryFilter(router.query.code);
-    const queryMessage = queryFilter(router.query.message);
-    const orderId = queryFilter(router.query.orderId);
-
-    // API 통신 에서 메세지 받을지 여기서 처리할지 결정
-    if (isBlank(message) || isBlank(queryMessage) || isBlank(orderId)) {
-      return;
-    }
+    const code = codecString(router.query.code);
+    const queryMessage = codecString(router.query.message);
+    const orderId = codecString(router.query.orderId);
 
     onApiPurchaseFail({ code, orderCode: orderId, message: queryMessage });
   }, [router.isReady]);
 
-  return <div>{message}</div>;
+  return (
+    <div>
+      {message} code: [{errorCode}]
+    </div>
+  );
 };
 
 export default FailPage;
