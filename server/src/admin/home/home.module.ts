f57e42722a4ac1934bd4a0022ed31d38c175a400
dlsdk0601 <dlsdk0601@gmail.com>
2023-12-19 01:12:19 +0900

    feat: typeorm imports 추가

--- server/src/admin/home/home.module.ts
+++ server/src/admin/home/home.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
-import { HomeService } from './home.service';
-import { HomeController } from './home.controller';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { HomeService } from "./home.service";
+import { HomeController } from "./home.controller";
+import { User } from "../../entities/user.entity";
+import { Subscribe } from "../../entities/subscribe.entity";
+import { PaymentCancelHistory, TossPaymentApprove } from "../../entities/payment-approve.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Subscribe, PaymentCancelHistory, TossPaymentApprove])],
   controllers: [HomeController],
-  providers: [HomeService]
+  providers: [HomeService],
 })
 export class HomeModule {}
