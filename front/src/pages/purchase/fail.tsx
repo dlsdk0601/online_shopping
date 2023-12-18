a35eec2631555a385a61905097ae470d09b269a2
dlsdk0601 <dlsdk0601@gmail.com>
2023-12-18 23:37:44 +0900

    refactor: 실패 페이지에 주문번호도 렌더링 추가

--- front/src/pages/purchase/fail.tsx
+++ front/src/pages/purchase/fail.tsx
@@ -9,6 +9,7 @@ import { FailPurchaseReq } from "../../api/type.g";
 const FailPage = () => {
   const router = useRouter();
   const [message, setMessage] = useState("");
+  const [orderCode, setOrderCode] = useState("");
   const [errorCode, setErrorCode] = useState("");
 
   const { mutate: onApiPurchaseFail } = useMutation(
@@ -21,7 +22,8 @@ const FailPage = () => {
 
         // front 에서 바로 처리 해도 되지만, 일반 API 통신과 같은 로직을 사용하기 위해 API 로 받아서 처리
         setMessage(res.message);
-        setErrorCode(res.code);
+        setErrorCode(res.errorCode);
+        setOrderCode(res.orderCode);
       },
     },
   );
@@ -40,7 +42,11 @@ const FailPage = () => {
 
   return (
     <div>
-      {message} code: [{errorCode}]
+      <p>주문 번호: {orderCode}</p>
+      <p>
+        {message} code: [{errorCode}]
+      </p>
+      <p>주문 번호와 code 를 캡쳐하시고 문의주시면 더 빠르게 답변드릴 수 있습니다.</p>
     </div>
   );
 };
