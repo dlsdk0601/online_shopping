876ec0bc40197e89c8e6602a4380ccf60b0806d7
dlsdk0601 <dlsdk0601@gmail.com>
2023-12-18 22:20:31 +0900

    feat: PurchaseStatus label 처리 생성

--- front/src/api/enum.ts
+++ front/src/api/enum.ts
@@ -1,11 +1,9 @@
-import { PaymentType, TossPaymentStatus, UserType } from "./enum.g";
+import { PaymentType, PurchaseItemStatus, TossPaymentStatus, UserType } from "./enum.g";
 
 export const userTypeLabelToEnum = (str: string | null | undefined): UserType => {
   switch (str) {
     case "GOOGLE":
       return UserType.GOOGLE;
-    case "APPLE":
-      return UserType.APPLE;
     case "KAKAO":
       return UserType.KAKAO;
     case "NAVER":
@@ -45,6 +43,29 @@ export const paymentStatusEnumToLabel = (value: TossPaymentStatus): string => {
       return "결제 기간 만료";
     case TossPaymentStatus.ABORTED:
     default:
-      return "결제 승인 실패";
+      return "결제 실패";
+  }
+};
+
+export const purchaseStatusEnumToLabel = (value: PurchaseItemStatus): string => {
+  switch (value) {
+    case PurchaseItemStatus.WAITING:
+      return "결제 대기";
+    case PurchaseItemStatus.IN_PROGRESS:
+      return "결제 진행중";
+    case PurchaseItemStatus.SUCCESS:
+      return "결제 완료";
+    case PurchaseItemStatus.CANCEL:
+      return "결제 취소";
+    case PurchaseItemStatus.REFUND_WAITING:
+      return "환불 진행중";
+    case PurchaseItemStatus.REFUND_SUCCESS:
+      return "환불 완료";
+    case PurchaseItemStatus.REFUND_FAIL:
+      return "환불 실패";
+    case PurchaseItemStatus.FAIL:
+      return "결제 실패";
+    default:
+      return "결제 실패";
   }
 };
